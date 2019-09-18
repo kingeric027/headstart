e783c08fdcaef7f3af017b424e71ab0815a95985
Oliver Heywood <oheywood@four51.com>
2019-09-18 14:33:49 -0500

    updaing IShopperContext so functions look like functions.

--- src/UI/Buyer/src/app/ocm-default-components/shopper-context.ts
+++ src/UI/Buyer/src/app/ocm-default-components/shopper-context.ts
@@ -21,65 +21,65 @@ export interface IShopperContext {
 
 export interface ICartActions {
   addToCartSubject: Observable<LineItem>;
-  addToCart: (lineItem: LineItem) => Promise<LineItem>;
-  removeLineItem: (lineItemID: string) => Promise<void>;
-  updateQuantity: (lineItemID: string, newQuantity: number) => Promise<LineItem>;
-  addManyToCart: (lineItem: LineItem[]) => Promise<LineItem[]>;
-  emptyCart: () => Promise<void>;
-  onAddToCart: (callback: (lineItem: LineItem) => void) => void;
+  addToCart(lineItem: LineItem): Promise<LineItem>;
+  removeLineItem(lineItemID: string): Promise<void>;
+  updateQuantity(lineItemID: string, newQuantity: number): Promise<LineItem>;
+  addManyToCart(lineItem: LineItem[]): Promise<LineItem[]>;
+  emptyCart(): Promise<void>;
+  onAddToCart(callback: (lineItem: LineItem) => void): void;
 }
 
 export interface IRouteActions {
-  onUrlChange: (callback: (path: string) => void) => void;
-  toProductDetails: (productID: string) => void;
-  toProductList: (options?: ProductFilters) => void;
-  toCheckout: () => void;
-  toHome: () => void;
-  toCart: () => void;
-  toLogin: () => void;
-  toRegister: () => void;
-  toMyProfile: () => void;
-  toMyAddresses: () => void;
-  toMyPaymentMethods: () => void;
-  toMyOrders: () => void;
-  toOrdersToApprove: () => void;
+  onUrlChange(callback: (path: string) => void): void;
+  toProductDetails(productID: string): void;
+  toProductList(options?: ProductFilters): void;
+  toCheckout(): void;
+  toHome(): void;
+  toCart(): void;
+  toLogin(): void;
+  toRegister(): void;
+  toMyProfile(): void;
+  toMyAddresses(): void;
+  toMyPaymentMethods(): void;
+  toMyOrders(): void;
+  toOrdersToApprove(): void;
 }
 
 export interface ICurrentUser {
   user: MeUser;
-  onUserChange: (callback: (user: User) => void) => void;
-  onIsAnonymousChange: (callback: (isAnonymous: boolean) => void) => void;
-  onFavoriteProductsChange: (callback: (productIDs: string[]) => void) => void;
-  setIsFavoriteProduct: (isFav: boolean, productID: string) => void;
-  onFavoriteOrdersChange: (callback: (orderIDs: string[]) => void) => void;
-  setIsFavoriteOrder: (isFav: boolean, orderID: string) => void;
+  onUserChange(callback: (user: User) => void): void;
+  onIsAnonymousChange(callback: (isAnonymous: boolean) => void): void;
+  onFavoriteProductsChange(callback: (productIDs: string[]) => void): void;
+  setIsFavoriteProduct(isFav: boolean, productID: string): void;
+  onFavoriteOrdersChange(callback: (orderIDs: string[]) => void): void;
+  setIsFavoriteOrder(isFav: boolean, orderID: string): void;
 }
 
 export interface ICurrentOrder {
   order: Order;
   lineItems: ListLineItem;
-  onOrderChange: (callback: (order: Order) => void) => void;
-  onLineItemsChange: (callback: (lineItems: ListLineItem) => void) => void;
+  onOrderChange(callback: (order: Order) => void): void;
+  onLineItemsChange(callback: (lineItems: ListLineItem) => void): void;
 }
 
 export interface IProductFilterActions {
-  toPage: (pageNumber: number) => void;
-  sortBy: (field: string) => void;
-  clearSort: () => void;
-  searchBy: (searchTerm: string) => void;
-  clearSearch: () => void;
-  filterByFacet: (field: string, value: string, isFacet?: boolean) => void;
-  clearFacetFilter: (field: string) => void;
-  filterByCategory: (categoryID: string) => void;
-  clearCategoryFilter: () => void;
-  filterByFavorites: (showOnlyFavorites: boolean) => void;
-  clearAllFilters: () => void;
-  onFiltersChange: (callback: (filters: ProductFilters) => void) => void;
+  toPage(pageNumber: number): void;
+  sortBy(field: string): void;
+  clearSort(): void;
+  searchBy(searchTerm: string): void;
+  clearSearch(): void;
+  filterByFacet(field: string, value: string, isFacet?: boolean): void;
+  clearFacetFilter(field: string): void;
+  filterByCategory(categoryID: string): void;
+  clearCategoryFilter(): void;
+  filterByFavorites(showOnlyFavorites: boolean): void;
+  clearAllFilters(): void;
+  onFiltersChange(callback: (filters: ProductFilters) => void): void;
 }
 
 export interface IAuthActions {
-  login: (username: string, password: string) => void;
-  logout: () => void;
+  login(username: string, password: string): void;
+  logout(): void;
 }
 
 export interface ProductFilters {
