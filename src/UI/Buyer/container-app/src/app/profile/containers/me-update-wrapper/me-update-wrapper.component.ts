a6b59e29e1e57851c77246341e8080ae75cb4c42
Oliver Heywood <oheywood@four51.com>
2019-10-04 14:49:03 -0500

    and this

--- src/UI/Buyer/container-app/src/app/profile/containers/me-update-wrapper/me-update-wrapper.component.ts
+++ src/UI/Buyer/container-app/src/app/profile/containers/me-update-wrapper/me-update-wrapper.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ShopperContextService } from 'src/app/shared/services/shopper-context/shopper-context.service';
 
 @Component({
-  templateUrl: `<ocm-profile-me-update [context]="context"></ocm-profile-me-update>
+  template: `
+    <ocm-profile-me-update [context]="context"></ocm-profile-me-update>
   `,
 })
 export class MeUpdateWrapperComponent implements OnInit {
