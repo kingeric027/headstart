3cd7da6481974ed2bd24598cb95174c1b83bb34e
Oliver Heywood <oheywood@four51.com>
2019-11-15 09:40:30 -0600

    Change header config

--- src/UI/Seller/src/app/layout/header/header.component.ts
+++ src/UI/Seller/src/app/layout/header/header.component.ts
@@ -39,7 +39,7 @@ export class HeaderComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.headerConfig = SupplierHeaderConfig;
+    this.headerConfig = MarketMangagerHeaderConfig;
   }
 
   logout() {
