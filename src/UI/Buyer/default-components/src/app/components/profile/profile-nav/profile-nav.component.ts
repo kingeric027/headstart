790a692b98643a20f09dd83c182c5c7bbe6333ab
Oliver Heywood <oheywood@four51.com>
2019-12-09 14:51:58 -0600

    fix this also

--- src/UI/Buyer/default-components/src/app/components/profile/profile-nav/profile-nav.component.ts
+++ src/UI/Buyer/default-components/src/app/components/profile/profile-nav/profile-nav.component.ts
@@ -1,8 +1,13 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import { ShopperContextService } from 'marketplace';
 
 @Component({
   templateUrl: './profile-nav.component.html',
   styleUrls: ['./profile-nav.component.scss'],
   encapsulation: ViewEncapsulation.ShadowDom,
 })
-export class OCMProfileNav {}
+export class OCMProfileNav {
+  constructor(public context: ShopperContextService) {}
+}
+
+
