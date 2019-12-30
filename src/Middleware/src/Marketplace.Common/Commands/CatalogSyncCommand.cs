41093f4accc1b5fa4bbcf9b817fddfb23eaafe3f
Steve Davis <sdavis@four51.com>
2019-12-30 12:39:19 -0600

    changes for Catalog creation due to impersonation of supplier user needs. bump helper library version

---
+++ src/Middleware/src/Marketplace.Common/Commands/CatalogSyncCommand.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Marketplace.Common.Exceptions;
+using Marketplace.Common.Models;
+using Marketplace.Common.Queries;
+using OrderCloud.SDK;
+
+namespace Marketplace.Common.Commands
+{
+    public class CatalogSyncCommand : SyncCommand, IWorkItemCommand
+    {
+        private readonly IOrderCloudClient _oc;
+        public CatalogSyncCommand(AppSettings settings, LogQuery log, IOrderCloudClient oc) : base(settings, log)
+        {
+            _oc = oc;
+        }
+
+        public async Task<JObject> CreateAsync(WorkItem wi)
+        {
+            var obj = wi.Current.ToObject<Catalog>();
+            try
+            {
+                await _oc.Catalogs.CreateAsync(obj, wi.Token);
+                return JObject.FromObject(obj);
+            }
+            catch (OrderCloudException exId) when (IdExists(exId))
+            {
+                // handle 409 errors by refreshing cache
+                await _log.Save(new OrchestrationLog(wi)
+                {
+                    ErrorType = OrchestrationErrorType.CreateExistsError,
+                    Message = exId.Message,
+                    Level = LogLevel.Error
+                });
+                return await GetAsync(wi);
+            }
+            catch (OrderCloudException ex)
+            {
+                await _log.Save(new OrchestrationLog(wi)
+                {
+                    ErrorType = OrchestrationErrorType.CreateGeneralError,
+                    Message = ex.Message,
+                    Level = LogLevel.Error
+                });
+                throw new Exception(OrchestrationErrorType.CreateGeneralError.ToString(), ex);
+            }
+            catch (Exception e)
+            {
+                await _log.Save(new OrchestrationLog(wi)
+                {
+                    ErrorType = OrchestrationErrorType.CreateGeneralError,
+                    Message = e.Message,
+                    Level = LogLevel.Error
+                });
+                throw new Exception(OrchestrationErrorType.CreateGeneralError.ToString(), e);
+            }
+        }
+
+        public async Task<JObject> UpdateAsync(WorkItem wi)
+        {
+            var obj = JObject.FromObject(wi.Current).ToObject<Catalog>();
+            try
+            {
+                await _oc.Catalogs.SaveAsync<Catalog>(wi.RecordId, obj, wi.Token);
+                return JObject.FromObject(obj);
+            }
+            catch (OrderCloudException ex)
+            {
+                await _log.Save(new OrchestrationLog(wi)
+                {
+                    ErrorType = OrchestrationErrorType.UpdateGeneralError,
+                    Message = ex.Message,
+                    Level = LogLevel.Error
+                });
+                throw new Exception(OrchestrationErrorType.UpdateGeneralError.ToString(), ex);
+            }
+        }
+
+        public async Task<JObject> PatchAsync(WorkItem wi)
+        {
+            var obj = JObject.FromObject(wi.Diff).ToObject<PartialCatalog>();
+            try
+            {
+                await _oc.Catalogs.PatchAsync<Catalog>(wi.RecordId, obj, wi.Token);
+                return JObject.FromObject(obj);
+            }
+            catch (OrderCloudException ex)
+            {
+                await _log.Save(new OrchestrationLog(wi)
+                {
+                    ErrorType = OrchestrationErrorType.PatchGeneralError,
+                    Message = ex.Message,
+                    Level = LogLevel.Error
+                });
+                throw new Exception(OrchestrationErrorType.PatchGeneralError.ToString(), ex);
+            }
+        }
+
+        public Task<JObject> DeleteAsync(WorkItem wi)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<JObject> GetAsync(WorkItem wi)
+        {
+            try
+            {
+                var response = await _oc.Catalogs.GetAsync<Catalog>(wi.RecordId, wi.Token);
+                return JObject.FromObject(response);
+            }
+            catch (OrderCloudException ex)
+            {
+                await _log.Save(new OrchestrationLog(wi)
+                {
+                    ErrorType = OrchestrationErrorType.GetGeneralError,
+                    Message = ex.Message,
+                    Level = LogLevel.Error
+                });
+                throw new Exception(OrchestrationErrorType.GetGeneralError.ToString(), ex);
+            }
+        }
+    }
+}
