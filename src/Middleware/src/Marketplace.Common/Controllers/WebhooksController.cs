188e1b3ef665bdf0b62373453af7994bc46c7609
Sydney <scasey@four51.com>
2020-01-08 10:42:18 -0600

    order submit webhook and password reset message

--- src/Middleware/src/Marketplace.Common/Controllers/WebhooksController.cs
+++ src/Middleware/src/Marketplace.Common/Controllers/WebhooksController.cs
@@ -1,10 +1,29 @@
-﻿using System;
+﻿using Marketplace.Common.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using OrderCloud.SDK;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Marketplace.Common.Controllers
 {
-    class Class1
+    public class WebhooksController : BaseController
     {
+        public WebhooksController(AppSettings settings) : base(settings)
+        {
+        }
+
+        [HttpPost, Route("ordersubmit")]
+        public void HandleOrderSubmitWebhook([FromBody] WebhookPayloads.Orders.Submit payload)
+        {
+            Console.WriteLine("order submit webhook received");
+        }
+        [HttpPost, Route("passwordreset")]
+        public void HandleResetPassword([FromBody] MessageNotification payload)
+        {
+            Console.WriteLine("password reset message received");
+        }
     }
 }
