33e8734b6811688e392ea19b9155b430a347e100
DJ Steinmetz <dsteinmetz@four51.com>
2020-01-15 13:36:32 -0600

    add ShipFromAddressID field on product form

--- src/UI/Seller/src/app/products/product-edit/product-edit.component.ts
+++ src/UI/Seller/src/app/products/product-edit/product-edit.component.ts
@@ -1,12 +1,15 @@
-import { Component, Input, Output, EventEmitter, ChangeDetectorRef, OnChanges } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ChangeDetectorRef, OnChanges, OnInit } from '@angular/core';
 import { get as _get } from 'lodash';
 import { FormGroup } from '@angular/forms';
+import { Address, ListAddress, OcSupplierAddressService, MeUser } from '@ordercloud/angular-sdk';
+import { SupplierAddressService } from '@app-seller/shared/services/supplier/supplier-address.service';
+import { CurrentUserService } from '@app-seller/shared/services/current-user/current-user.service';
 @Component({
   selector: 'app-product-edit',
   templateUrl: './product-edit.component.html',
   styleUrls: ['./product-edit.component.scss'],
 })
-export class ProductEditComponent {
+export class ProductEditComponent implements OnInit {
   @Input()
   resourceForm: FormGroup;
   @Input()
@@ -14,6 +17,24 @@ export class ProductEditComponent {
   @Output()
   updateResource = new EventEmitter<any>();
   hasVariations = false;
+  @Input()
+  supplierAddresses: ListAddress;
+
+  constructor(
+    private supplierAddressService: SupplierAddressService,
+    private currentUserService: CurrentUserService,
+    private ocSupplierAddressService: OcSupplierAddressService
+  ) {}
+
+  ngOnInit() {
+    // TODO: Eventually move to a resolve so that they are there before the component instantiates.
+    this.getSupplierAddresses();
+  }
+
+  async getSupplierAddresses(): Promise<void> {
+    const user: MeUser = await this.currentUserService.getUser();
+    this.supplierAddresses = await this.ocSupplierAddressService.List(user.Supplier.ID).toPromise();
+  }
 
   updateResourceFromEvent(event: any, field: string): void {
     this.updateResource.emit({ value: event.target.value, field });
--- src/UI/Seller/src/app/products/product-table/product-table.component.ts
+++ src/UI/Seller/src/app/products/product-table/product-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectorRef, NgZone } from '@angular/core';
+import { Component, ChangeDetectorRef, NgZone, OnInit } from '@angular/core';
 import { Product } from '@ordercloud/angular-sdk';
 import { ProductService } from '@app-seller/shared/services/product/product.service';
 import { ResourceCrudComponent } from '@app-seller/shared/components/resource-crud/resource-crud.component';
@@ -11,13 +11,12 @@ function createProductForm(product: Product) {
     Description: new FormControl(product.Description),
     Inventory: new FormControl(product.Inventory),
     QuantityMultiplier: new FormControl(product.QuantityMultiplier),
+    // TODO: Make required once platform updates compatibility for supplier address on this field
     ShipFromAddressID: new FormControl(product.ShipFromAddressID),
     ShipHeight: new FormControl(product.ShipHeight),
     ShipWidth: new FormControl(product.ShipWidth),
     ShipLength: new FormControl(product.ShipLength),
     ShipWeight: new FormControl(product.ShipWeight),
-    // SpecCount: new FormControl(product.SpecCount),
-    // VariantCount: new FormControl(product.VariantCount),
     xp: new FormControl(product.xp),
   });
 }
@@ -26,7 +25,7 @@ function createProductForm(product: Product) {
   templateUrl: './product-table.component.html',
   styleUrls: ['./product-table.component.scss'],
 })
-export class ProductTableComponent extends ResourceCrudComponent<Product> {
+export class ProductTableComponent extends ResourceCrudComponent<Product> implements OnInit {
   constructor(
     private productService: ProductService,
     changeDetectorRef: ChangeDetectorRef,
