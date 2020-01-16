7a2c0c8f6be0ea0020af720042c106cad5d1b01b
Bill Hickey <bhickey@four51.com>
2020-01-15 19:08:43 -0600

    product route mocks beginning

--- src/UI/Seller/src/app/shared/services/product/product.service.ts
+++ src/UI/Seller/src/app/shared/services/product/product.service.ts
@@ -4,13 +4,28 @@ import { transform as _transform, pickBy as _pickBy } from 'lodash';
 import { cloneDeep as _cloneDeep } from 'lodash';
 import { OcProductService, ListProduct, Product } from '@ordercloud/angular-sdk';
 import { ResourceCrudService } from '../resource-crud/resource-crud.service';
+import { MarketPlaceProduct } from '@app-seller/shared/models/MarketPlaceProduct.interface';
 
 // TODO - this service is only relevent if you're already on the product details page. How can we enforce/inidcate that?
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService extends ResourceCrudService<Product> {
-  constructor(router: Router, activatedRoute: ActivatedRoute, ocProductsService: OcProductService) {
+  constructor(router: Router, activatedRoute: ActivatedRoute, private ocProductsService: OcProductService) {
     super(router, activatedRoute, ocProductsService, '/products', 'products');
+  }
+
+  async getMarketPlaceProductDetailByID(productID: string): Promise<MarketPlaceProduct> {
+    const marketPlaceProduct: MarketPlaceProduct = await this.ocProductsService.Get(productID).toPromise();
+    // just returning the oc product for now
+    return marketPlaceProduct;
+  }
+
+  async saveMarketPlaceProduct(marketPlaceProduct: MarketPlaceProduct): Promise<MarketPlaceProduct> {
+    this.ocProductsService.Create(marketPlaceProduct);
+    this.listResources();
+
+    // mocking the return value of the create marketplace product route
+    return marketPlaceProduct;
   }
 }
