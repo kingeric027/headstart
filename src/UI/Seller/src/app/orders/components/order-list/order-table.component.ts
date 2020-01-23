f692012a73bb51d50cb588f16f84fa25fb915a81
Sydney <scasey@four51.com>
2020-01-23 17:57:47 -0600

    SEB-367 Order direction filters working - need to finish button styling

--- src/UI/Seller/src/app/orders/components/order-list/order-table.component.ts
+++ src/UI/Seller/src/app/orders/components/order-list/order-table.component.ts
@@ -5,6 +5,8 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { SellerUserService } from '@app-seller/shared/services/seller-user/seller-user.service';
 import { FormControl, FormGroup } from '@angular/forms';
 import { OrderService } from '@app-seller/shared/services/order/order.service';
+import { AppAuthService } from '@app-seller/auth/services/app-auth.service';
+import { SELLER } from '@app-seller/shared/models/ordercloud-user.types';
 
 @Component({
   selector: 'app-order-table',
@@ -12,13 +14,21 @@ import { OrderService } from '@app-seller/shared/services/order/order.service';
   styleUrls: ['./order-table.component.scss'],
 })
 export class OrderTableComponent extends ResourceCrudComponent<Order> {
+  shouldShowOrderToggle = false;
+  buttonType: string;
   constructor(
     private orderService: OrderService,
     changeDetectorRef: ChangeDetectorRef,
     router: Router,
     activatedroute: ActivatedRoute,
-    ngZone: NgZone
+    ngZone: NgZone,
+    private appAuthService: AppAuthService
   ) {
     super(changeDetectorRef, orderService, router, activatedroute, ngZone);
+    this.shouldShowOrderToggle = this.appAuthService.getOrdercloudUserType() === SELLER;
+  }
+  setOrderDirection(direction: string) {
+    this.orderService.addFilters({ OrderDirection: direction });
+    this.buttonType = direction;
   }
 }
