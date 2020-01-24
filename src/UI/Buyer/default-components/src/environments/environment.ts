d03822426b88ea77c4dc2f9219d644182d14e258
Oliver Heywood <oheywood@four51.com>
2020-01-24 15:50:27 -0600

    undo change

--- src/UI/Buyer/default-components/src/environments/environment.ts
+++ src/UI/Buyer/default-components/src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   clientID: '26B45BD1-5D02-489D-A60E-3B37C852606C',
   marketplaceID: 'SEB',
   baseUrl: 'https://localhost:4200',
-  middlewareUrl: 'http://localhost:51100',
+  middlewareUrl: 'https://marketplace-api-qa.azurewebsites.net',
   cmsUrl: 'https://s3.dualstack.us-east-1.amazonaws.com/staticcintas.eretailing.com/images/product',
   ssoLink: 'https://stage-authorize.anytimefitness.com/authorize?response_type=code&client_id=86d70db9-22e6-47ba-a1ab-bbe00c9b6451&redirect_uri=https://selfesteembrands-api-qa.azurewebsites.net/authorize',
 };
