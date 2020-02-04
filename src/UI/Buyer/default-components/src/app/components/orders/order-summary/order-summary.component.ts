d850c392ae16e7f7bc6b8c8fb3f2b407aa85f74c
Max Maher <mmaher@four51.com>
2020-02-04 14:59:51 -0600

    Adding more logic to shipping display

--- src/UI/Buyer/default-components/src/app/components/orders/order-summary/order-summary.component.ts
+++ src/UI/Buyer/default-components/src/app/components/orders/order-summary/order-summary.component.ts
@@ -1,20 +1,28 @@
 import { CurrencyPipe } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { MarketplaceOrder } from 'marketplace';
 
 @Component({
   templateUrl: './order-summary.component.html',
   styleUrls: ['./order-summary.component.scss'],
 })
-export class OCMOrderSummary {
+export class OCMOrderSummary implements OnChanges {
+  shippingSelected: boolean = false;
+  orderLocalVar: MarketplaceOrder;
   @Input() order: MarketplaceOrder;
   @Input() checkoutPanel = '';
+
+  ngOnChanges() {
+    if (this.orderLocalVar.ShippingCost !== 0 && this.shippingSelected !== true) {
+      this.shippingSelected = true;
+    };
+  }
 
   displayTax() {
     if (this.checkoutPanel === 'cart') {
       return 'Calculated during checkout';
     }
-    if (this.checkoutPanel === 'shippingAddress' || this.checkoutPanel === 'shippingSelection') {
+    if (this.checkoutPanel === 'shippingAddress' && !this.shippingSelected || this.checkoutPanel === 'shippingSelection' && !this.shippingSelected) {
       return 'Pending selections';
     }
     return new CurrencyPipe('en-US').transform(this.order.TaxCost);
@@ -24,14 +32,25 @@ export class OCMOrderSummary {
     if (this.checkoutPanel === 'cart') {
       return 'Calculated during checkout';
     }
-    if (this.checkoutPanel === 'shippingAddress' || this.checkoutPanel === 'shippingSelection') {
+    if (this.checkoutPanel === 'shippingAddress' && !this.shippingSelected && !this.orderLocalVar) {
+      this.orderLocalVar = this.order;
+      this.orderLocalVar.ShippingCost = 0;
+    }
+    if (this.checkoutPanel === 'shippingAddress' && !this.shippingSelected || this.checkoutPanel === 'shippingSelection' && !this.shippingSelected) {
       return 'Pending selections';
     }
+    this.orderLocalVar.ShippingCost = this.order.ShippingCost;
     return new CurrencyPipe('en-US').transform(this.order.ShippingCost);
   }
 
   displayTotal() {
     if (this.checkoutPanel === 'cart') {
+      return new CurrencyPipe('en-US').transform(this.order.Subtotal);
+    }
+    if (this.checkoutPanel === 'shippingAddress' && !this.shippingSelected) {
+      return new CurrencyPipe('en-US').transform(this.order.Subtotal);
+    }
+    if (this.checkoutPanel === 'shippingSelection' && !this.shippingSelected) {
       return new CurrencyPipe('en-US').transform(this.order.Subtotal);
     }
     return new CurrencyPipe('en-US').transform(this.order.Total);
