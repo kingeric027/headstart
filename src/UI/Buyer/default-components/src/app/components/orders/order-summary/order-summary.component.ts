9864717d30c0b0b54778d395bb5ba7371cc67e81
Max Maher <mmaher@four51.com>
2020-02-05 10:22:48 -0600

    Eliminated unnecessary code (onChanges)

--- src/UI/Buyer/default-components/src/app/components/orders/order-summary/order-summary.component.ts
+++ src/UI/Buyer/default-components/src/app/components/orders/order-summary/order-summary.component.ts
@@ -1,25 +1,15 @@
 import { CurrencyPipe } from '@angular/common';
-import { Component, Input, OnChanges } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MarketplaceOrder } from 'marketplace';
 
 @Component({
   templateUrl: './order-summary.component.html',
   styleUrls: ['./order-summary.component.scss'],
 })
-export class OCMOrderSummary implements OnChanges {
+export class OCMOrderSummary {
   shippingSelected: boolean;
-  orderLocalVar: MarketplaceOrder;
   @Input() order: MarketplaceOrder;
   @Input() checkoutPanel = '';
-
-  //If the user has selected a shipping option, the shipping selected property should be true.
-  ngOnChanges() {
-    if (this.orderLocalVar) {
-      if (this.orderLocalVar.ShippingCost !== 0 && this.shippingSelected !== true) {
-        this.shippingSelected = true;
-      };
-    }
-  }
 
   displayTax() {
     if (this.checkoutPanel === 'cart') {
@@ -35,14 +25,10 @@ export class OCMOrderSummary implements OnChanges {
     if (this.checkoutPanel === 'cart') {
       return 'Calculated during checkout';
     }
-    if (this.checkoutPanel === 'shippingAddress' && !this.shippingSelected && !this.orderLocalVar) {
-      this.orderLocalVar = this.order;
-      this.orderLocalVar.ShippingCost = 0;
-    }
     if (this.checkoutPanel === 'shippingAddress' && !this.shippingSelected || this.checkoutPanel === 'shippingSelection' && !this.shippingSelected) {
       return 'Pending selections';
     }
-    this.orderLocalVar.ShippingCost = this.order.ShippingCost;
+    this.shippingSelected = true;
     return new CurrencyPipe('en-US').transform(this.order.ShippingCost);
   }
 
