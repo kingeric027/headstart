b60eae2dd9c5aa6063941ccca44e3acc9389c6e2
Sydney <scasey@four51.com>
2020-02-05 17:19:41 -0600

    SEB-446: Supplier info is displayed on line-item table

--- src/UI/Buyer/default-components/src/app/components/cart/lineitem-table/lineitem-table.component.ts
+++ src/UI/Buyer/default-components/src/app/components/cart/lineitem-table/lineitem-table.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { LineItem } from '@ordercloud/angular-sdk';
-import { groupBy as _groupBy, map as _map, without as _without } from 'lodash';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { Address, LineItem, OcSupplierAddressService, OcSupplierService, Supplier } from '@ordercloud/angular-sdk';
+import { groupBy as _groupBy } from 'lodash';
 import { ShopperContextService } from 'marketplace';
 import { getPrimaryImageUrl } from 'src/app/services/images.helpers';
 
@@ -13,15 +13,37 @@ export class OCMLineitemTable implements OnInit {
   closeIcon = faTimes;
   @Input() lineItems: LineItem[];
   @Input() readOnly: boolean;
+  supplierInfo: Supplier[] = [];
+  supplierAddresses: Address[] = [];
   liGroupedByShipFrom: LineItem[][];
   liGroups: any;
 
-  constructor(private context: ShopperContextService) { }
+  constructor(private context: ShopperContextService, private ocSupplierService: OcSupplierService, private ocSupplierAddressService: OcSupplierAddressService) { }
 
   async ngOnInit() {
     await this.lineItems;
     this.liGroups = _groupBy(this.lineItems, li => li.ShipFromAddressID);
     this.liGroupedByShipFrom = Object.values(this.liGroups);
+    await this.getSuppliers();
+  }
+
+  getSuppliers() {
+    this.liGroupedByShipFrom.forEach(async group => {
+      let address = await this.ocSupplierAddressService.Get(group[0].SupplierID, group[0].ShipFromAddressID).toPromise();
+      let info = await this.ocSupplierService.Get(group[0].SupplierID).toPromise();
+      this.supplierAddresses.push(address);
+      this.supplierInfo.push(info);
+    });
+  }
+
+  formatPhoneNumber(supplierPhone: string) {
+    var cleaned = ('' + supplierPhone).replace(/\D/g, '')
+    var match = cleaned.match(/^(1|)?(\d{3})(\d{3})(\d{4})$/)
+    if (match) {
+      var intlCode = (match[1] ? '+1 ' : '')
+      return [intlCode, '(', match[2], ') ', match[3], '-', match[4]].join('')
+    }
+    return null
   }
 
   removeLineItem(lineItemID: string) {
--- src/UI/Buyer/default-components/src/app/config/app.config.ts
+++ src/UI/Buyer/default-components/src/app/config/app.config.ts
@@ -3,7 +3,7 @@ import { environment } from 'src/environments/environment';
 
 export const ocAppConfig: AppConfig = {
   appname: environment.appname,
-  clientID:  environment.clientID,
+  clientID: environment.clientID,
   marketplaceID: environment.marketplaceID,
   baseUrl: environment.baseUrl,
   middlewareUrl: environment.middlewareUrl,
@@ -16,13 +16,14 @@ export const ocAppConfig: AppConfig = {
     'MeAdmin',
     'MeCreditCardAdmin',
     'MeXpAdmin',
-    'Shopper', 
+    'Shopper',
     'BuyerReader',
     'PasswordReset',
     'SupplierReader',
+    'SupplierAddressReader',
     // TODO - remove. In the platform, to create a payment with a saved credit card,
     // you need both access to the credit card and OrderAdmin. Don't go live with OrderAdmin
     // still on this list haha. 
-    'OrderAdmin'   
+    'OrderAdmin'
   ]
 };
