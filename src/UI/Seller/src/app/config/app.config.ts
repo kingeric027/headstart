795039d4720f43966e77a0d9bcf2cfa47b425ba1
DJ Steinmetz <dsteinmetz@four51.com>
2020-02-18 16:37:36 -0600

    Add new user permission to app scope

--- src/UI/Seller/src/app/config/app.config.ts
+++ src/UI/Seller/src/app/config/app.config.ts
@@ -66,7 +66,7 @@ export const ocAppConfig: AppConfig = {
     'MPMeSupplierAdmin',
     'MPMeSupplierAddressAdmin',
     'MPMeSupplierUserAdmin',
-    'MPMeSupplierUserGroupAdmin',
+    'MPSupplierUserGroupAdmin',
   ],
 };
 
