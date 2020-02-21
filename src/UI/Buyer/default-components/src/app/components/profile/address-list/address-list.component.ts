b8fab41b9254b6d7d00a1b962c672497d86aff3d
Sydney <scasey@four51.com>
2020-02-21 13:58:38 -0600

    Getting & displaying suggested addresses

--- src/UI/Buyer/default-components/src/app/components/profile/address-list/address-list.component.ts
+++ src/UI/Buyer/default-components/src/app/components/profile/address-list/address-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { faPlus, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
-import { ListBuyerAddress, BuyerAddress } from '@ordercloud/angular-sdk';
+import { ListBuyerAddress, BuyerAddress, ListAddress } from '@ordercloud/angular-sdk';
 import { faTrashAlt, faEdit } from '@fortawesome/free-regular-svg-icons';
 import { ModalState } from '../../../models/modal-state.class';
 import { ShopperContextService } from 'marketplace';
@@ -24,8 +24,8 @@ export class OCMAddressList implements OnInit {
   areYouSureModal = ModalState.Closed;
   showCreateAddressForm = false;
   isLoading = false;
-
-  constructor(private context: ShopperContextService) {}
+  suggestedAddresses: ListAddress;
+  constructor(private context: ShopperContextService) { }
 
   ngOnInit(): void {
     this.reloadAddresses();
@@ -74,12 +74,12 @@ export class OCMAddressList implements OnInit {
     await this.context.currentUser.addresses.delete(address.ID);
     this.addresses.Items = this.addresses.Items.filter(a => a.ID !== address.ID);
   }
-  
+
   updateRequestOptions(newOptions: { page?: number; search?: string }): void {
     this.requestOptions = Object.assign(this.requestOptions, newOptions);
     this.reloadAddresses();
   }
-  
+
 
   protected refresh(): void {
     this.currentAddress = null;
@@ -87,21 +87,31 @@ export class OCMAddressList implements OnInit {
   }
 
   private async addAddress(address: BuyerAddress): Promise<void> {
-    address.Shipping = true;
-    address.Billing = true;
-    const newAddress = await this.context.currentUser.addresses.create(address);
-    this.addresses.Items = [...this.addresses.Items, newAddress];
-    this.showCreateAddressForm = false;
-    this.refresh();
+    try {
+      address.Shipping = true;
+      address.Billing = true;
+      const newAddress = await this.context.currentUser.addresses.create(address);
+      this.addresses.Items = [...this.addresses.Items, newAddress];
+      this.showCreateAddressForm = false;
+      this.refresh();
+    } catch (ex) {
+      this.suggestedAddresses = ex.error.Errors[0].Data.Body.SuggestedValidAddresses;
+      throw ex;
+    }
   }
-  
+
   private async updateAddress(address: BuyerAddress): Promise<any> {
-    address.ID = this.currentAddress.ID;
-    await this.context.currentUser.addresses.edit(address.ID, address);
-    this.showCreateAddressForm = false;
-    this.refresh();
+    try {
+      address.ID = this.currentAddress.ID;
+      await this.context.currentUser.addresses.edit(address.ID, address);
+      this.showCreateAddressForm = false;
+      this.refresh();
+    } catch (ex) {
+      this.suggestedAddresses = ex.error.Errors[0].Data.Body.SuggestedValidAddresses;
+      throw ex;
+    }
   }
-  
+
   private async reloadAddresses(): Promise<void> {
     this.isLoading = true;
     this.addresses = await this.context.currentUser.addresses.list(this.requestOptions);
