9ac0fec4e33758f8f46a9fb77c131911f367d0c6
Bill Hickey <bhickey@four51.com>
2020-02-24 11:40:14 -0600

    add buyer creation command and implement in seller

--- src/UI/Seller/src/app/buyers/components/buyers/buyer.service.ts
+++ src/UI/Seller/src/app/buyers/components/buyers/buyer.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { OcBuyerService, Buyer } from '@ordercloud/angular-sdk';
 import { ResourceCrudService } from '@app-seller/shared/services/resource-crud/resource-crud.service';
+import { MiddlewareAPIService } from '@app-seller/shared/services/middleware-api/middleware-api.service';
 
 export const BUYER_SUB_RESOURCE_LIST = ['users', 'locations', 'payments', 'approvals', 'categories'];
 
@@ -10,7 +11,19 @@ export const BUYER_SUB_RESOURCE_LIST = ['users', 'locations', 'payments', 'appro
   providedIn: 'root',
 })
 export class BuyerService extends ResourceCrudService<Buyer> {
-  constructor(router: Router, activatedRoute: ActivatedRoute, ocBuyerService: OcBuyerService) {
+  constructor(
+    router: Router,
+    activatedRoute: ActivatedRoute,
+    ocBuyerService: OcBuyerService,
+    private middleware: MiddlewareAPIService
+  ) {
     super(router, activatedRoute, ocBuyerService, '/buyers', 'buyers', BUYER_SUB_RESOURCE_LIST);
+  }
+
+  async createNewResource(resource: any): Promise<any> {
+    const newBuyer = await this.middleware.createBuyer(resource);
+    this.resourceSubject.value.Items = [...this.resourceSubject.value.Items, newBuyer];
+    this.resourceSubject.next(this.resourceSubject.value);
+    return newBuyer;
   }
 }
