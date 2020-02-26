7014e668c41b93eff1db61352bbb1d0647929f75
Sydney <scasey@four51.com>
2020-02-26 14:35:24 -0600

    Fixed address suggestion UI (buyer)

--- src/UI/Buyer/default-components/src/app/components/layout/address-suggestions/address-suggestion.component.ts
+++ src/UI/Buyer/default-components/src/app/components/layout/address-suggestions/address-suggestion.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { BuyerAddress, ListBuyerAddress, ShopperContextService } from 'marketplace';
+import { faSquare, faCheckSquare } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   templateUrl: './address-suggestion.component.html',
@@ -10,6 +11,8 @@ export class OCMAddressSuggestion implements OnInit {
   @Input() suggestedAddresses: ListBuyerAddress;
   @Output() selectedAddress = new EventEmitter<BuyerAddress>();
   activeAddress: BuyerAddress;
+  faSquare = faSquare;
+  faCheckSquare = faCheckSquare;
   constructor(public context: ShopperContextService) { }
 
   ngOnInit() {
