0407c21030d88c1cf7ce708a803eda0925c0198a
DJ Steinmetz <dsteinmetz@four51.com>
2020-03-04 12:28:58 -0600

    Add buyer location middleware routes

---
+++ src/Middleware/src/Marketplace.Models/Models/Marketplace/MarketplaceBuyerLocation.cs
@@ -0,0 +1,26 @@
+﻿using OrderCloud.SDK;
+
+namespace Marketplace.Models
+{
+    public class MarketplaceBuyerLocation
+    {
+        public MarketplaceBuyerUserGroup UserGroup { get; set; }
+        public MarketplaceBuyerAddress Address { get; set; }
+    }
+    public class MarketplaceBuyerUserGroup : UserGroup<MarketplaceUserGroupXp>, IMarketplaceObject
+    {
+
+    }
+    public class MarketplaceBuyerAddress : Address<MarketplaceBuyerAddressXp>, IMarketplaceObject
+    {
+        
+    }
+    public class MarketplaceUserGroupXp
+    {
+        public string Type { get; set; }
+    }
+    public class MarketplaceBuyerAddressXp
+    {
+        public string Email { get; set; }
+    }
+}
