8b1ea7ba7f8d7944a0c2f94721f2b512eeca6b5e
Sydney <scasey@four51.com>
2020-03-05 13:22:41 -0600

    Displaying quote orders appropriately for sellers

--- src/UI/Seller/src/app/orders/components/order-details/order-details.component.ts
+++ src/UI/Seller/src/app/orders/components/order-details/order-details.component.ts
@@ -3,6 +3,7 @@ import { Order, LineItem, OcLineItemService, OcPaymentService, Payment, Address 
 import { groupBy as _groupBy } from 'lodash';
 import { MarketPlaceProductImage } from '@app-seller/shared/models/MarketPlaceProduct.interface';
 import { getProductMainImageUrlOrPlaceholder } from '@app-seller/products/product-image.helper';
+import { OrderService } from '@app-seller/orders/order.service';
 
 @Component({
   selector: 'app-order-details',
@@ -25,7 +26,7 @@ export class OrderDetailsComponent {
       this.handleSelectedOrderChange(order);
     }
   }
-  constructor(private ocLineItemService: OcLineItemService, private ocPaymentService: OcPaymentService) {}
+  constructor(private ocLineItemService: OcLineItemService, private ocPaymentService: OcPaymentService, private orderService: OrderService) { }
 
   setCardType(payment) {
     if (!payment.xp.cardType || payment.xp.cardType === null) {
@@ -48,6 +49,10 @@ export class OrderDetailsComponent {
   getIncomingOrOutgoing() {
     const url = window.location.href;
     url.includes('Outgoing') ? (this.orderDirection = 'Outgoing') : (this.orderDirection = 'Incoming');
+  }
+
+  isQuoteOrder(order: Order) {
+    return this.orderService.isQuoteOrder(order);
   }
 
   private async handleSelectedOrderChange(order: Order): Promise<void> {
