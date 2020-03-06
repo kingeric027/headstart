53fbba9030b0e7353c11965cfb4cf23ca1ce6718
DJ Steinmetz <dsteinmetz@four51.com>
2020-03-06 10:26:35 -0600

    :rickandmorty: remove console log

--- src/UI/Seller/src/app/shared/services/address-suggestion.helper.ts
+++ src/UI/Seller/src/app/shared/services/address-suggestion.helper.ts
@@ -3,7 +3,6 @@ import { ListBuyerAddress } from '@ordercloud/angular-sdk';
 export const getSuggestedAddresses = (ex): ListBuyerAddress => {
   for (const err of ex.error) {
     if (err.ErrorCode === 'blocked by web hook') {
-        console.log(err.Data?.Body?.SuggestedAddresses)
       return err.Data?.Body?.SuggestedAddresses;
     }
   }
