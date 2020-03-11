a4bfa330df2ed676a0e008fadf8b689da0804eaf
Bill Hickey <bhickey@four51.com>
2020-03-11 18:41:03 -0500

    add new order xp fields to seller app

--- src/UI/Seller/src/app/shared/models/MarketPlaceOrder.interface.ts
+++ src/UI/Seller/src/app/shared/models/MarketPlaceOrder.interface.ts
@@ -1,22 +1,24 @@
-import { Order } from '@ordercloud/angular-sdk'
+import { Order } from '@ordercloud/angular-sdk';
 
 export type MarketplaceOrder = Order<MarketplaceOrderXp>;
 
 export enum OrderType {
-    Quote = 'Quote',
-    Standard = 'Standard'
+  Quote = 'Quote',
+  Standard = 'Standard',
 }
 
 export interface MarketplaceOrderXp {
-    AvalaraTaxTransactionCode: string;
-    OrderType: OrderType;
-    QuoteOrderInfo: QuoteOrderInfo;
+  AvalaraTaxTransactionCode: string;
+  NeedsAttention: boolean;
+  StopShipSync: boolean;
+  OrderType: OrderType;
+  QuoteOrderInfo: QuoteOrderInfo;
 }
 
 export interface QuoteOrderInfo {
-    FirstName: string;
-    LastName: string;
-    Phone: string;
-    Email: string;
-    Comments?: string;
+  FirstName: string;
+  LastName: string;
+  Phone: string;
+  Email: string;
+  Comments?: string;
 }
