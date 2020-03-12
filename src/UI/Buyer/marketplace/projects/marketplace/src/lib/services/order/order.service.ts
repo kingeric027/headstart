ef51559f7e89ea7ca48507854fb1031057e28816
Bill Hickey <bhickey@four51.com>
2020-03-12 17:49:52 -0500

    clean order IDs

--- src/UI/Buyer/marketplace/projects/marketplace/src/lib/services/order/order.service.ts
+++ src/UI/Buyer/marketplace/projects/marketplace/src/lib/services/order/order.service.ts
@@ -1,6 +1,6 @@
 // angular
 import { Injectable } from '@angular/core';
-import { MarketplaceOrder } from '../../shopper-context';
+import { MarketplaceOrder, AppConfig } from '../../shopper-context';
 import { OrderStateService } from './order-state.service';
 import { CartService, ICart } from './cart.service';
 import { CheckoutService, ICheckout } from './checkout.service';
@@ -22,15 +22,28 @@ export class CurrentOrderService implements ICurrentOrder {
   onChange = this.state.onOrderChange.bind(this.state);
   reset = this.state.reset.bind(this.state);
 
-  constructor(public cart: CartService, public checkout: CheckoutService, private state: OrderStateService, private ocLineItemService: OcLineItemService, private ocOrderService: OcOrderService) { }
+  constructor(
+    public cart: CartService,
+    public checkout: CheckoutService,
+    private state: OrderStateService,
+    private ocLineItemService: OcLineItemService,
+    private ocOrderService: OcOrderService,
+    private appConfig: AppConfig
+  ) {}
 
   get(): MarketplaceOrder {
     return this.state.order;
   }
   async submitQuoteOrder(orderDetails: Order, lineItem: LineItem): Promise<Order> {
+    orderDetails.ID = `${this.appConfig.marketplaceID}{orderIncrementor}`;
     const quoteOrder = await this.ocOrderService.Create('Outgoing', orderDetails).toPromise();
     await this.ocLineItemService.Create('Outgoing', quoteOrder.ID, lineItem).toPromise();
-    const submittedQuoteOrder = await this.ocOrderService.Submit('Outgoing', quoteOrder.ID).toPromise().then(order => { return order });
+    const submittedQuoteOrder = await this.ocOrderService
+      .Submit('Outgoing', quoteOrder.ID)
+      .toPromise()
+      .then(order => {
+        return order;
+      });
     return submittedQuoteOrder;
   }
 }
