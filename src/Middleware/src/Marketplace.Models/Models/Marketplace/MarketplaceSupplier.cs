2001fba51818baee1788b8ecaaf24f6efd5e2464
Bill Hickey <bhickey@four51.com>
2020-03-16 15:23:32 -0500

    update supplier xp model

--- src/Middleware/src/Marketplace.Models/Models/Marketplace/MarketplaceSupplier.cs
+++ src/Middleware/src/Marketplace.Models/Models/Marketplace/MarketplaceSupplier.cs
@@ -15,8 +15,6 @@ namespace Marketplace.Models.Models.Marketplace
         public List<ProductImage> Images { get; set; } = new List<ProductImage>();
         public Contact SupportContact { get; set; }
         public bool SyncFreightPop { get; set; }
-        public string ApiClient { get; set; }
+        public string ApiClientID { get; set; }
     }
-
-    
 }
