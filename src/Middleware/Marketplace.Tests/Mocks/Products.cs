549147d05308c75de32169f47202d8415aa73b06
Sydney <scasey@four51.com>
2020-03-20 10:38:03 -0500

    Auto stash before merge of "SEB-507" and "OC-Marketplace/SEB-507"

---
+++ src/Middleware/Marketplace.Tests/Mocks/Products.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Marketplace.Common.Services;
+using Marketplace.Models;
+using Marketplace.Models.Extended;
+using OrderCloud.SDK;
+
+namespace Marketplace.Tests.Mocks
+{
+    public static class Products
+    {
+        public static MarketplaceProduct ProductWithOneImage()
+        {
+            return new MarketplaceProduct()
+            {
+                xp = new ProductXp()
+                {
+                    Images = new List<ProductImage>()
+                    {
+                        new ProductImage()
+                        {
+                            URL = "mock-image-url-0"
+                        }
+                    }
+                }
+            };
+        }
+
+        public static MarketplaceProduct PatchResponse()
+        {
+            var product = new MarketplaceProduct();
+            product.ID = "patched-product";
+            return product;
+        }
+    }
+}
---
+++ src/Middleware/Marketplace.Tests/Tests/TaxCodeMapperTests.cs
@@ -0,0 +1,46 @@
+﻿
+using Avalara.AvaTax.RestClient;
+using Marketplace.Common.Mappers.Avalara;
+using Marketplace.Common.Services.AvaTax.Models;
+using Marketplace.Helpers;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using Orchestration.Tests.Mocks;
+using OrderCloud.SDK;
+using System.Collections;
+
+namespace Marketplace.Tests
+{
+    public class TaxCodeMapperTests
+    {
+        [Test]
+		[TestCase(5, 100, 100, 400)]
+		[TestCase(1, 100, 100, 0)]
+		public void map_oc_list_args_to_avalara(int page, int pageSize, int expectedTop, int expectedSkip)
+        {
+			var result = TaxCodeMapper.Map(new ListArgs<TaxCodeModel>() { Filters = null, Page = page, PageSize = pageSize });
+			Assert.AreEqual(expectedTop, result.Top);
+			Assert.AreEqual(expectedSkip, result.Skip);
+		}
+
+		[Test]
+		[TestCase(0, 1, 1)]
+		[TestCase(1, 1, 2)]
+		[TestCase(100, 1, 101)]
+		public void map_avalara_list_args_to_oc(int skip, int top, int page)
+		{
+			var avalaraTaxCodesFromApiCall = TaxCodes.taxCodeObjectFromAvalaraFirstRecord();
+			var args = new TaxCodeListArgs() { Skip = skip, Top = top };
+			var result = TaxCodeMapper.Map(avalaraTaxCodesFromApiCall, args);
+			var expectedMeta = new ListPageMeta
+			{
+				Page = page,
+				PageSize = 100,
+				TotalCount = 1,
+			};
+			Assert.AreEqual(expectedMeta.Page, result.Meta.Page);
+			Assert.AreEqual(expectedMeta.PageSize, result.Meta.PageSize);
+			Assert.AreEqual(expectedMeta.TotalCount, result.Meta.TotalCount);
+		}
+	}
+}
