3f788fbe43934e2b9a61ac69b06d0649833558d9
Bill Hickey <bhickey@four51.com>
2020-04-10 11:43:13 -0500

    start api endpoint

---
+++ src/Middleware/src/Marketplace.Common/Controllers/OrderController.cs
@@ -0,0 +1,34 @@
+﻿using Marketplace.Common.Commands;
+using Marketplace.Models;
+using Marketplace.Models.Models.Marketplace;
+using Microsoft.AspNetCore.Mvc;
+using OrderCloud.SDK;
+using System.Threading.Tasks;
+using Marketplace.Helpers.Attributes;
+using Marketplace.Models.Attributes;
+using Marketplace.Models.Misc;
+
+namespace Marketplace.Common.Controllers
+{
+    [DocComments("\"Marketplace Orders\" for handling order commands in Marketplace")]
+    [MarketplaceSection.Marketplace(ListOrder = 2)]
+    [Route("order")]
+    public class OrderController : BaseController
+    {
+        
+        private readonly IOrderCommand _command;
+        public OrderController(IOrderCommand command, AppSettings settings) : base(settings)
+        {
+            _command = command;
+        }
+
+        [DocName("POST Acknowledge Quote Order")]
+        // todo update auth
+        [HttpPost]
+        [Route("acknowledgequote/{orderID}")]
+        public async Task AcknowledgeQuoteOrder(string orderID)
+        {
+            await _command.AcknowledgeQuoteOrder(orderID);
+        } 
+    }
+}
