84b3bc8fc6d5f1bb6ac708f8ea1e27f0a2427a55
Oliver Heywood <oheywood@four51.com>
2020-04-15 13:56:14 -0500

    Feel decent about this version

--- src/Middleware/src/Marketplace.Models/Models/Misc/ContentManagment.cs
+++ src/Middleware/src/Marketplace.Models/Models/Misc/ContentManagment.cs
@@ -6,37 +6,48 @@ using System.Text;
 
 namespace Marketplace.Models.Models.Misc
 {
-	public enum AssetType { Image, Unclassified }
+	public enum AssetType { Image, Theme, Attachment, StructuredData }
 
 	public class Asset: CosmosObject
 	{
-		public string Url { get; set; } // Settable to support external storage.
+		[Required]
+		public string AssetStorageID { get; set; } // Don't need to set or return. Only goes into building the Url.
+		public string Url { get; set; } // Settable to support external storage. Generated if not set. 
 		public string Title { get; set; }
 		public List<string> Tags { get; set; }
-		public AssetType Type { get; set; } = AssetType.Unclassified;
+		public AssetType Type { get; set; }
 		public string FileName { get; set; } // Defaults to the file name in the upload. Or should be required?
 		[ApiReadOnly]
 		public AssetMetadata Metadata { get; set; }
+	}
+
+	public class AssetAssignment: CosmosObject
+	{
+		[Required]
+		public string AssetID { get; set; }
+		[Required]
+		public string ResourceID { get; set; }
+		[Required]
+		public string ResourceType { get; set; }
+		public int AssetListOrder { get; set; } // Wthin the content of a single oc resource 
 	}
 
 	public class AssetMetadata
 	{
 		public string ContentType { get; set; }
 		public int SizeBytes { get; set; }
+		public bool IsStoredExternally { get; set; } // true if Url was set, false if it was generated
 		public int? ImageHeight { get; set; } = null; // null if asset not image
 		public int? ImageWidth { get; set; } = null; // null if asset not image
 	}
 
-	public class AssetGroup : CosmosObject
+	public class AssetStorage: CosmosObject
 	{
-		public string Name { get; set; }
-		public ResourceType ResourceType { get; set; }
-		public string ResourceID { get; set; }           // Each asset group is part of exactly 1 OC Resource. Is this ok? many-to-many assignment instead?
-		public int SizeBytes { get; set; } = 0;
-		public int? MaxSizeBytes { get; set; } = null;
-		public int? MaxAssetCount { get; set; } = null;
-		public AssetType? RestrictAssetType { get; set; } = null;
-		public List<string> AssetIDs { get; set; }       // have a route where these are expanded to include the full object, graphQL-like
+		public int AssetCount { get; set; } // generated
+		public int SizeBytes { get; set; } // generated
+		public int MaxiumumSizeBytes { get; set; }
+		public string ContainerName { get; set; } // "Assets-{SellerID}"
+		public string BlobStorageBaseUrl { get; set; }
 	}
 
 	public enum ResourceType
@@ -44,6 +55,20 @@ namespace Marketplace.Models.Models.Misc
 		Product, Category, Catalog, Promotion, Supplier, Buyer, BuyerAddress, SupplierAddrress,
 		AdminAddress, BuyerUserGroup, SupplierUserGroup, AdminUserGroup, Facet
 	}
+
+	//public class AssetGroup : CosmosObject
+	//{
+	//	public string Name { get; set; }
+	//	public ResourceType ResourceType { get; set; }
+	//	public string ResourceID { get; set; }           // Each asset group is part of exactly 1 OC Resource. Is this ok? many-to-many assignment instead?
+	//	public int SizeBytes { get; set; } = 0;
+	//	public int? MaxSizeBytes { get; set; } = null;
+	//	public int? MaxAssetCount { get; set; } = null;
+	//	public AssetType? RestrictAssetType { get; set; } = null;
+	//	public List<string> AssetIDs { get; set; }       // have a route where these are expanded to include the full object, graphQL-like
+	//}
+
+
 
 
 	//public class JSONContent: CosmosObject
