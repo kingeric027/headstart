ba105c6328c74a577b0cc3b02449e7c1fab3c800
Max Maher <mmaher@four51.com>
2020-04-21 11:51:40 -0500

    Disable ability to Delete when Supplier Location in use by Product

--- src/UI/Seller/src/app/suppliers/components/locations/supplier-location-edit/supplier-location-edit.component.ts
+++ src/UI/Seller/src/app/suppliers/components/locations/supplier-location-edit/supplier-location-edit.component.ts
@@ -1,12 +1,14 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnChanges } from '@angular/core';
 import { FormGroup } from '@angular/forms';
-import { ListAddress } from '@ordercloud/angular-sdk';
+import { ListAddress, OcProductService } from '@ordercloud/angular-sdk';
+import { ProductService } from '@app-seller/products/product.service';
 @Component({
     selector: 'app-supplier-location-edit',
     templateUrl: './supplier-location-edit.component.html',
     styleUrls: ['./supplier-location-edit.component.scss'],
 })
-export class SupplierLocationEditComponent {
+export class SupplierLocationEditComponent implements OnChanges {
+    locationHasNoProducts: boolean;
     @Input()
     resourceForm: FormGroup;
     @Input()
@@ -17,6 +19,27 @@ export class SupplierLocationEditComponent {
     updateResource = new EventEmitter<any>();
     @Output()
     selectAddress = new EventEmitter<any>();
+    @Output()
+    canDelete = new EventEmitter<boolean>();
+
+    constructor(private ocProductService: OcProductService) {}
+
+    ngOnChanges() {
+        this.setLocationID();
+    }
+
+    private setLocationID(): void {
+        const url = window.location.href;
+        const splitUrl = url.split('/');
+        const endUrl = splitUrl[splitUrl.length - 1];
+        this.determineIfDeletable(endUrl);
+    }
+
+    private async determineIfDeletable(locationID: string): Promise<void> {
+        const productList = await this.ocProductService.List({pageSize: 1, filters: {ShipFromAddressID: locationID}}).toPromise();
+        this.locationHasNoProducts = productList.Items.length ? false : true;
+        this.canDelete.emit(this.locationHasNoProducts);
+    }
 
     updateResourceFromEvent(event: any, field: string): void {
         this.updateResource.emit({ value: event.target.value, field });
