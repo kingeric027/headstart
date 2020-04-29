ecf63aa3345b0fe2f6dedf4c0132dd3b8d59f64b
Oliver Heywood <oheywood@four51.com>
2020-04-29 14:45:28 -0500

    field needs accessors

--- src/Middleware/src/Marketplace.Models/Models/ContentManagement/Image.cs
+++ src/Middleware/src/Marketplace.Models/Models/ContentManagement/Image.cs
@@ -19,6 +19,6 @@ namespace Marketplace.Common.Models
         public DateTimeOffset timeStamp { get; set; }
         public string URL { get; set; }
         public int ListOrder { get; set; }
-        public List<string> Tags = new List<string>();
+		public List<string> Tags { get; set; } = new List<string>();
     }
 }
