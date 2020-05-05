5ce54d8c370f672e914b99c3d26dac9ad83bf5d8
DJ Steinmetz <dsteinmetz@four51.com>
2020-05-05 16:35:01 -0500

    Get available exchange rates for supplier edit

--- src/UI/Seller/src/app/suppliers/components/suppliers/supplier-edit/supplier-edit.component.ts
+++ src/UI/Seller/src/app/suppliers/components/suppliers/supplier-edit/supplier-edit.component.ts
@@ -1,18 +1,27 @@
-import { Component, Input, Output, EventEmitter, ChangeDetectorRef, OnChanges } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ChangeDetectorRef, OnChanges, OnInit } from '@angular/core';
 import { get as _get } from 'lodash';
 import { FormGroup } from '@angular/forms';
+import { MiddlewareAPIService } from '@app-seller/shared/services/middleware-api/middleware-api.service';
+import { Rates } from '@app-seller/shared/models/exchange-rates.interface';
 @Component({
   selector: 'app-supplier-edit',
   templateUrl: './supplier-edit.component.html',
   styleUrls: ['./supplier-edit.component.scss'],
 })
-export class SupplierEditComponent {
+export class SupplierEditComponent implements OnInit {
   @Input()
   resourceForm: FormGroup;
   @Input()
   filterConfig;
   @Output()
   updateResource = new EventEmitter<any>();
+  availableCurrencies: Rates[] = [];
+  constructor(public middleware: MiddlewareAPIService) {}
+
+  async ngOnInit(): Promise<void> {
+    this.availableCurrencies = (await this.middleware.getAvailableCurrencies('USD')).Rates;
+  }
+
   updateResourceFromEvent(event: any, field: string): void {
     const value = field === 'Active' || field === 'xp.SyncFreightPop' ? event.target.checked : event.target.value;
     this.updateResource.emit({ value, field });
