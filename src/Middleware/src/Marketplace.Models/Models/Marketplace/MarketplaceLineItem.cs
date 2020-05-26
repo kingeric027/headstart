61dabaed405f03f089dc6043c10580c08a10c39e
DJ Steinmetz <dsteinmetz@four51.com>
2020-05-26 16:11:06 -0500

    Move Currency to Order xp

--- src/Middleware/src/Marketplace.Models/Models/Marketplace/MarketplaceLineItem.cs
+++ src/Middleware/src/Marketplace.Models/Models/Marketplace/MarketplaceLineItem.cs
@@ -10,7 +10,6 @@ namespace Marketplace.Models.Models.Marketplace
 	public class LineItemXp {
         public LineItemReturnInfo LineItemReturnInfo { get; set; }
         public decimal? ProductUnitPrice { get; set; }
-        public string OrderCurrency { get; set; }
     }
 
     public class LineItemReturnInfo
