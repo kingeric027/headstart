527c09fd1a17866a3c0c12ea7eb00bfa1e22f59b
Steve Davis <sdavis@four51.com>
2020-05-26 20:07:29 -0500

    old swagger gen

--- src/Middleware/src/Marketplace.Common/Controllers/SwaggerController.cs
+++ src/Middleware/src/Marketplace.Common/Controllers/SwaggerController.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json.Linq;
 using ordercloud.integrations.extensions;
+using ordercloud.integrations.openapispec;
 using ErrorCodes = Marketplace.Models.ErrorCodes;
 
 namespace Marketplace.Common.Controllers
@@ -18,20 +19,19 @@ namespace Marketplace.Common.Controllers
         public async Task<JObject> Get()
         {
             var reference = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
-            //var g = new OpenApiGenerator<BaseController, OrderCloudIntegrationsAuthAttribute>()
-            //    .CollectMetaData(Path.Combine(reference, "reference.md"), ErrorCodes.All)
-            //    .DefineSpec(new SwaggerConfig()
-            //    {
-            //        Name = "Marketplace",
-            //        ContactEmail = "oheywood@four51.com",
-            //        Description = "Marketplace API",
-            //        Host = "https://marketplace-api-qa.azurewebsites.net",
-            //        Title = "Marketplace API",
-            //        Url = "https://ordercloud.io",
-            //        Version = "1.0"
-            //    });
-            //return await Task.FromResult(g.Specification());
-            return new JObject();
+            var g = new OpenApiGenerator<BaseController, OrderCloudIntegrationsAuthAttribute>()
+                .CollectMetaData(Path.Combine(reference, "reference.md"), ErrorCodes.All)
+                .DefineSpec(new SwaggerConfig()
+                {
+                    Name = "Marketplace",
+                    ContactEmail = "oheywood@four51.com",
+                    Description = "Marketplace API",
+                    Host = "https://marketplace-api-qa.azurewebsites.net",
+                    Title = "Marketplace API",
+                    Url = "https://ordercloud.io",
+                    Version = "1.0"
+                });
+            return await Task.FromResult(g.Specification());
         }
     }
 }
