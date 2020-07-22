63cfb34de7ca25f0723bffa0b5c6ff77369d0e3b
Bill Hickey <bhickey@four51.com>
2020-07-22 11:56:50 -0500

    change category config route to filter route

---
+++ src/Middleware/src/Marketplace.Common/Models/Marketplace/SupplierFilterConfig.cs
@@ -0,0 +1,41 @@
+﻿using OrderCloud.SDK;
+using System;
+using System.Collections.Generic;
+using ordercloud.integrations.library;
+using ordercloud.integrations.cms;
+using Newtonsoft.Json.Linq;
+
+namespace Marketplace.Models
+{
+    [SwaggerModel]
+    public class SupplierFilterConfigDocument : Document<SupplierFilterConfig>
+    {
+    }
+
+    [SwaggerModel]
+    public class SupplierFilterConfig
+    {
+        public string Display { get; set; }
+        public string Path { get; set; }
+        public List<Filter> Items { get; set; }
+        public bool AllowSupplierEdit { get; set; }
+        public bool AllowSellerEdit { get; set; }
+        public BuyerAppFilterType BuyerAppFilterType { get; set; }
+    }
+
+    [SwaggerModel]
+    public class Filter
+    {
+        public string Text {get; set;}
+        public string Value {get; set; }
+    }
+
+    public enum BuyerAppFilterType
+    {
+       SelectOption,
+       NonUI
+    }
+
+}
+
+
