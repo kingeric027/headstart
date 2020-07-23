5c2a3edd8f2c560c210d40e4b5d8c7fd6b517a9d
Sydney <scasey@four51.com>
2020-07-23 17:32:21 -0500

    Created model for kits

---
+++ src/Middleware/src/Marketplace.Common/Models/Marketplace/MarketplaceKit.cs
@@ -0,0 +1,37 @@
+﻿using ordercloud.integrations.cms;
+using ordercloud.integrations.library;
+using OrderCloud.SDK;
+using System.Collections.Generic;
+
+namespace Marketplace.Common.Models.Marketplace
+{
+    [SwaggerModel]
+    public class KitProductDocument : Document<KitProduct> { }
+    [SwaggerModel]
+    public class MarketplaceKitProductDocument : Document<MarketplaceKitProduct> { }
+
+    [SwaggerModel]
+    public class MarketplaceKitProduct
+    {
+        public Product Product { get; set; }
+        public IList<AssetForDelivery> Images { get; set; }
+        public IList<AssetForDelivery> Attachments { get; set; }
+        public KitProduct ProductAssignments { get; set; }
+    }
+
+    [SwaggerModel]
+    public class KitProduct
+    {
+        public IList<ProductInKit> ProductsInKit { get; set; }
+    }
+
+    [SwaggerModel]
+    public class ProductInKit
+    {
+        public string ID { get; set; }
+        public bool Required { get; set; }
+        public int MinQty { get; set; }
+        public int MaxQty { get; set; }
+    }
+
+}
