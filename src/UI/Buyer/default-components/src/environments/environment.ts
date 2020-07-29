2bbe31c00a2e3d725468cde0dcf4a159351d4740
DJ Steinmetz <dsteinmetz@four51.com>
2020-07-29 16:58:46 -0500

    Remove commented code

--- src/UI/Buyer/default-components/src/environments/environment.ts
+++ src/UI/Buyer/default-components/src/environments/environment.ts
@@ -94,25 +94,3 @@ const devEnvironments = {
   },
 };
 export const environment: Environment = devEnvironments[appName];
-
-// export const environment: Environment = {
-//   hostedApp: false,
-//   appname: 'Marketplace Local',
-//   clientID: 'A5231DF1-2B00-4002-AB40-738A9E2CEC4B',
-//   marketplaceID: 'SEB',
-//   baseUrl: 'https://localhost:4200',
-//   middlewareUrl: middlewareUrls[middlewareLocationSelection],
-//   translateBlobUrl: 'https://marktplacetest.blob.core.windows.net/ngx-translate/i18n/',
-//   ssoLink:
-//     'https://stage-authorize.anytimefitness.com/authorize?response_type=code&client_id=86d70db9-22e6-47ba-a1ab-bbe00c9b6451&redirect_uri=https://selfesteembrands-api-qa.azurewebsites.net/authorize',
-//   ordercloudEnv: OrdercloudEnv.Staging,
-// };
-/*
-
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
