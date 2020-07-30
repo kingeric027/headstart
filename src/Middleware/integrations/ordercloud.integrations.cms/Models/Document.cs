74cb442a785d1d0bfa383a808264d4fe8e16a1dc
Oliver Heywood <oheywood@four51.com>
2020-07-30 13:29:20 -0500

    so far

---
+++ src/Middleware/integrations/ordercloud.integrations.cms/Models/Document.cs
@@ -0,0 +1,33 @@
+﻿using Cosmonaut.Attributes;
+using Newtonsoft.Json.Linq;
+using ordercloud.integrations.library;
+using OrderCloud.SDK;
+using Microsoft.Azure.Documents;
+using Newtonsoft.Json;
+using ordercloud.integrations.library.Cosmos;
+using System.Collections.ObjectModel;
+using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribute;
+
+namespace ordercloud.integrations.cms.Models
+{
+	[SwaggerModel]
+	public class Document
+	{
+		[Required]
+		public string ID { get; set; }
+		[Required]
+		public JObject Doc { get; set; }
+		[ApiReadOnly]
+		public string SchemaSpecUrl { get; set; }
+		[ApiReadOnly]
+		public History History { get; set; }
+	}
+
+	[SwaggerModel]
+	public class Document<TDoc> : Document
+	{
+		public new TDoc Doc { get; set; }
+
+	}
+
+}
