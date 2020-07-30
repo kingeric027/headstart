3103872643657ec92035a04d0ec3086b872d699b
Eric King <eking@four51.com>
2020-07-30 10:37:00 -0500

    Remove unused function

--- src/UI/Buyer/default-components/src/app/services/orderType.helper.ts
+++ src/UI/Buyer/default-components/src/app/services/orderType.helper.ts
@@ -1,10 +1,6 @@
 import { OrderType } from 'marketplace';
-import { MarketplaceOrder, MarketplaceLineItem } from '@ordercloud/headstart-sdk';
+import { MarketplaceOrder } from '@ordercloud/headstart-sdk';
 
 export const isQuoteOrder = (order?: MarketplaceOrder): boolean => {
   return order.xp.OrderType === OrderType.Quote;
 };
-
-export const lineItemHasBeenShipped = (lineItem: MarketplaceLineItem): boolean => {
-  return ['Complete', 'Returned', 'ReturnRequested'].includes(lineItem.xp.LineItemStatus);
-}
