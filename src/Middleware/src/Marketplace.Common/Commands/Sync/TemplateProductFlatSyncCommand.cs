b2b4ade8213fd15d664493aad3db06f7007c63e9
Steve Davis <sdavis@four51.com>
2020-08-06 14:56:43 -0500

    handle discrepancies between price and product existing.

--- src/Middleware/src/Marketplace.Common/Commands/Sync/TemplateProductFlatSyncCommand.cs
+++ src/Middleware/src/Marketplace.Common/Commands/Sync/TemplateProductFlatSyncCommand.cs
@@ -29,7 +29,7 @@ namespace Marketplace.Common.Commands
             try
             {
                 obj.ID = wi.RecordId;
-                var priceSchedule = await _oc.PriceSchedules.CreateAsync(new MarketplacePriceSchedule()
+                var priceSchedule = new MarketplacePriceSchedule()
                 {
                     ID = obj.ID,
                     ApplyShipping = obj.ApplyShipping,
@@ -37,11 +37,12 @@ namespace Marketplace.Common.Commands
                     MaxQuantity = obj.MaxQuantity,
                     MinQuantity = obj.MinQuantity,
                     Name = obj.Name,
-                    PriceBreaks = new List<PriceBreak>() { new PriceBreak() { Price = obj.Price.To<decimal>(), Quantity = obj.QuantityMultiplier } },
+                    PriceBreaks = new List<PriceBreak>()
+                        {new PriceBreak() {Price = obj.Price.To<decimal>(), Quantity = obj.QuantityMultiplier}},
                     RestrictedQuantity = obj.RestrictedQuantity,
                     UseCumulativeQuantity = obj.UseCumulativeQuantity
-                }, wi.Token);
-                var product = await _oc.Products.CreateAsync<MarketplaceProduct>(new MarketplaceProduct()
+                };
+                var product = new MarketplaceProduct()
                 {
                     Active = true,
                     AutoForward = false,
@@ -70,12 +71,25 @@ namespace Marketplace.Common.Commands
                             Unit = obj.UnitOfMeasure
                         }
                     }
-                }, wi.Token);
-                await _oc.Products.SaveAssignmentAsync(new ProductAssignment()
+                };
+                try
                 {
-                    ProductID = product.ID,
-                    PriceScheduleID = priceSchedule.ID
-                }, wi.Token);
+                    priceSchedule = await _oc.PriceSchedules.CreateAsync<MarketplacePriceSchedule>(priceSchedule, wi.Token);
+                }
+                catch (OrderCloudException exists) when (IdExists(exists))
+                {
+                    //continue on
+                }
+                finally
+                {
+                    product = await _oc.Products.CreateAsync<MarketplaceProduct>(product, wi.Token);
+                    await _oc.Products.SaveAssignmentAsync(new ProductAssignment()
+                    {
+                        ProductID = product.ID,
+                        PriceScheduleID = priceSchedule.ID
+                    }, wi.Token);
+                }
+                
                 return JObject.FromObject(Map(product, priceSchedule));
             }
             catch (OrderCloudException exId) when (IdExists(exId))
@@ -119,7 +133,7 @@ namespace Marketplace.Common.Commands
             try
             {
                 if (obj.ID == null) obj.ID = wi.RecordId;
-                var priceSchedule = await _oc.PriceSchedules.SaveAsync(wi.RecordId, new MarketplacePriceSchedule()
+                var priceSchedule = await _oc.PriceSchedules.SaveAsync<MarketplacePriceSchedule>(wi.RecordId, new MarketplacePriceSchedule()
                 {
                     ID = obj.ID,
                     ApplyShipping = obj.ApplyShipping,
@@ -180,7 +194,7 @@ namespace Marketplace.Common.Commands
             }
         }
 
-        private static TemplateProductFlat Map(MarketplaceProduct product, PriceSchedule priceSchedule)
+        private static TemplateProductFlat Map(MarketplaceProduct product, MarketplacePriceSchedule priceSchedule)
         {
             return new TemplateProductFlat
             {
@@ -217,7 +231,7 @@ namespace Marketplace.Common.Commands
             {
                 //TODO: partial mapping
                 var product = await _oc.Products.PatchAsync<MarketplaceProduct>(wi.RecordId, objProduct, wi.Token);
-                var priceSchedule = await _oc.PriceSchedules.PatchAsync(wi.RecordId, objPrice, wi.Token);
+                var priceSchedule = await _oc.PriceSchedules.PatchAsync<MarketplacePriceSchedule>(wi.RecordId, objPrice, wi.Token);
                 return JObject.FromObject(Map(product, priceSchedule));
             }
             catch (OrderCloudException ex)
@@ -242,7 +256,7 @@ namespace Marketplace.Common.Commands
             try
             {
                 var product = await _oc.Products.GetAsync<MarketplaceProduct>(wi.RecordId, wi.Token);
-                var priceSchedule = await _oc.PriceSchedules.GetAsync<PriceSchedule>(product.DefaultPriceScheduleID, wi.Token);
+                var priceSchedule = await _oc.PriceSchedules.GetAsync<MarketplacePriceSchedule>(product.DefaultPriceScheduleID, wi.Token);
                 //var _images = GetProductImages(id, user);
                 //var _attachments = GetProductAttachments(id, user);
                 return JObject.FromObject(Map(product, priceSchedule));
