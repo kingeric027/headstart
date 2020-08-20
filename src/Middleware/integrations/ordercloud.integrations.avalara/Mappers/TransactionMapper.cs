0ec05ebe6d7c554ed2817bac802e1e00eeddccf8
Oliver Heywood <oheywood@four51.com>
2020-08-20 10:35:22 -0500

    resale certs bring tax to 0 for those products

--- src/Middleware/integrations/ordercloud.integrations.avalara/Mappers/TransactionMapper.cs
+++ src/Middleware/integrations/ordercloud.integrations.avalara/Mappers/TransactionMapper.cs
@@ -8,50 +8,84 @@ namespace ordercloud.integrations.avalara
 {
 	public static class TransactionMapper
 	{
-		public static TransactionBuilder WithLineItem(this TransactionBuilder trans, LineItem lineItem)
+		public static CreateTransactionModel ToAvalaraTransationModel(this OrderWorksheet order, string companyCode, DocumentType docType)
 		{
-			var line = new TransactionLineModel()
+			var lines = new List<LineItemModel>();
+			foreach (var shipment in order.ShipEstimateResponse.ShipEstimates)
 			{
-				lineAmount = lineItem.LineTotal,
+				var (shipFrom, shipTo) = shipment.GetAddresses(order.LineItems);
+				var method = shipment.GetSelectedShippingMethod();
+				lines.Add(method.ToLineItemModel(shipFrom, shipTo));
+			}
+
+			var hasResaleCert = ((int?) order.Order.BillingAddress.xp.AvalaraCertificateID != null);
+			var exemptionNo = hasResaleCert ? order.Order.BillingAddress.ID : null;
+
+			foreach (var lineItem in order.LineItems) lines.Add(lineItem.ToLineItemModel(lineItem.ShipFromAddress, lineItem.ShippingAddress, exemptionNo));
+
+			return new CreateTransactionModel()
+			{
+				companyCode = companyCode,
+				type = docType,
+				customerCode = order.Order.FromCompanyID,
+				date = DateTime.Now,
+				lines = lines
+			};
+		}
+
+
+		private static LineItemModel ToLineItemModel(this LineItem lineItem, Address shipFrom, Address shipTo, string exemptionNo)
+		{
+			var line = new LineItemModel()
+			{
+				amount = lineItem.LineTotal,
 				taxCode = lineItem.Product.xp.Tax.Code,
 				itemCode = lineItem.ProductID,
 				customerUsageType = null,
-				lineNumber = lineItem.ID
+				number = lineItem.ID,
+				addresses = ToAddressesModel(shipFrom, shipTo)
 			};
-			return trans.WithLine(line, lineItem.ShipFromAddress, lineItem.ShippingAddress);
+			var isResaleProduct = (bool)lineItem.Product.xp.IsResale;
+			if (isResaleProduct && exemptionNo != null)
+			{
+				line.exemptionCode = exemptionNo;
+			}
+			return line;
 		}
 
-		public static TransactionBuilder WithShippingRate(this TransactionBuilder trans, ShipMethod method, Address shipFrom, Address shipTo)
+		private static LineItemModel ToLineItemModel(this ShipMethod method, Address shipFrom, Address shipTo)
 		{
-			var shipping = new TransactionLineModel()
+			return new LineItemModel()
 			{
-				lineAmount = method.Cost,
+				amount = method.Cost,
 				taxCode = "FR",
 				itemCode = method.Name,
 				customerUsageType = null,
-				lineNumber = method.ID,
+				number = method.ID,
+				addresses = ToAddressesModel(shipFrom, shipTo)
 			};
-			return trans.WithLine(shipping, shipFrom, shipTo);
 		}
 
-		private static TransactionBuilder WithLine(this TransactionBuilder trans, TransactionLineModel line)
+		private static AddressesModel ToAddressesModel(Address shipFrom, Address shipTo)
 		{
-			return trans.WithLine(line.lineAmount ?? 0, 1, line.taxCode, null, line.itemCode, line.customerUsageType, line.lineNumber);
+			return new AddressesModel()
+			{
+				shipFrom = shipFrom.ToAddressLocationInfo(),
+				shipTo = shipTo.ToAddressLocationInfo(),
+			};
 		}
 
-		private static TransactionBuilder WithLine(this TransactionBuilder trans, TransactionLineModel line, Address shipFrom, Address shipTo)
+		private static AddressLocationInfo ToAddressLocationInfo(this Address address)
 		{
-			return trans.WithLine(line).WithLineShipFrom(shipFrom).WithLineShipTo(shipTo);
-		}
-
-		private static TransactionBuilder WithLineShipTo(this TransactionBuilder trans, Address address)
-		{
-			return trans.WithLineAddress(TransactionAddressType.ShipTo, address.Street1, address.Street2, null, address.City, address.State, address.Zip, address.Country);
-		}
-
-		private static TransactionBuilder WithLineShipFrom(this TransactionBuilder trans, Address address)
-		{
-			return trans.WithLineAddress(TransactionAddressType.ShipFrom, address.Street1, address.Street2, null, address.City, address.State, address.Zip, address.Country);
+			return new AddressLocationInfo()
+			{
+				line1 = address.Street1,
+				line2 = address.Street2,
+				city = address.City,
+				region = address.State,
+				postalCode = address.Zip,
+				country = address.Country
+			};
 		}
 	}
 }
