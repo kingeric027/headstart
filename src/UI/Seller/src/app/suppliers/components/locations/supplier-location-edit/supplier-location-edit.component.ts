689dcd89a8ff6ca0018bc5266d41b10d0fd79ee3
DJ Steinmetz <dsteinmetz@four51.com>
2020-08-25 20:58:31 -0500

    Supplier Location refactor

--- src/UI/Seller/src/app/suppliers/components/locations/supplier-location-edit/supplier-location-edit.component.ts
+++ src/UI/Seller/src/app/suppliers/components/locations/supplier-location-edit/supplier-location-edit.component.ts
@@ -1,18 +1,42 @@
-import { Component, EventEmitter, Input, Output, OnChanges } from '@angular/core';
-import { FormGroup } from '@angular/forms';
-import { Address, OcProductService, ListPage } from '@ordercloud/angular-sdk';
-import { ProductService } from '@app-seller/products/product.service';
+import { Component, EventEmitter, Input, Output, OnChanges, SimpleChanges } from '@angular/core';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
+import { Address, ListPage } from '@ordercloud/angular-sdk';
 import { MiddlewareAPIService } from '@app-seller/shared/services/middleware-api/middleware-api.service';
 import { ActivatedRoute } from '@angular/router';
+import { SupportedCountries, GeographyConfig } from '@app-seller/shared/models/supported-countries.interface';
+import { ValidateCAZip, ValidateUSZip, ValidatePhone } from '@app-seller/validators/validators';
+import { takeWhile } from 'rxjs/operators';
+import { SupplierAddressService } from '../supplier-address.service';
+import { OrderCloudIntegrationsConversionRate, HeadStartSDK } from '@ordercloud/headstart-sdk';
 @Component({
   selector: 'app-supplier-location-edit',
   templateUrl: './supplier-location-edit.component.html',
   styleUrls: ['./supplier-location-edit.component.scss'],
 })
 export class SupplierLocationEditComponent implements OnChanges {
+  alive = true;
   locationHasNoProducts: boolean;
-  @Input()
+  countryOptions: SupportedCountries[];
+  flag: string;
   resourceForm: FormGroup;
+  zipLabel: string = 'Zip Code';
+  zipPlaceholder: string = 'Enter Zip';
+  stateLabel: string = 'State';
+  statePlaceholder: string = 'Enter State';
+  cityLabel: string = 'City';
+  cityPlaceholder: string = 'Enter City';
+  street1Label: string = 'Street 1';
+  street1Placeholder: string = 'Enter Street 1';
+  countryHasBeenSelected: boolean = false;
+  currencyOptions: OrderCloudIntegrationsConversionRate[];
+  @Input()
+  set location(supplierLocation: Address) {
+    if (supplierLocation.ID) {
+      this.handleSelectedLocationChange(supplierLocation);
+    } else {
+      this.handleSelectedLocationChange(this.supplierLocationSerivce.emptyResource);
+    }
+  }
   @Input()
   filterConfig;
   @Input()
@@ -24,10 +48,67 @@ export class SupplierLocationEditComponent implements OnChanges {
   @Output()
   canDelete = new EventEmitter<boolean>();
 
-  constructor(private middleware: MiddlewareAPIService, private activatedRoute: ActivatedRoute) {}
+  constructor(
+    private middleware: MiddlewareAPIService,
+    private activatedRoute: ActivatedRoute,
+    private supplierLocationSerivce: SupplierAddressService
+  ) {
+    this.countryOptions = GeographyConfig.getCountries();
+  }
 
   ngOnChanges() {
     this.setLocationID();
+  }
+
+  async handleSelectedLocationChange(location: Address): Promise<void> {
+    // Bug with the SDK where GetRateList has wrong return type
+    try {
+      HeadStartSDK.ExchangeRates.GetRateList().then(res => {
+        this.currencyOptions = res.Items;
+        location.Country && this.setFlag(location.Country);
+      })
+    } finally {
+      this.createSupplierLocationForm(location);
+    }
+  }
+
+  createSupplierLocationForm(supplierLocation: Address) {
+    this.resourceForm = new FormGroup({
+      AddressName: new FormControl(supplierLocation.AddressName, Validators.required),
+      CompanyName: new FormControl(supplierLocation.CompanyName, Validators.required),
+      Street1: new FormControl(supplierLocation.Street1, Validators.required),
+      Street2: new FormControl(supplierLocation.Street2),
+      City: new FormControl(supplierLocation.City, Validators.required),
+      State: new FormControl(supplierLocation.State, Validators.required),
+      Zip: new FormControl({value: supplierLocation.Zip, disabled: true}, [Validators.required, ValidateUSZip || ValidateCAZip]),
+      Country: new FormControl(supplierLocation.Country, Validators.required),
+      Phone: new FormControl(supplierLocation.Phone, ValidatePhone),
+    });
+    this.setZipValidator();
+  }
+
+  setFlag(locationCountry: string) {
+    const currency = this.getCurrencyFromCode(locationCountry);
+    this.flag = this.getFlagForCountry(currency);
+  }
+
+  setZipValidator() {
+    const zipControl = this.resourceForm.get('Zip');
+    this.countryHasBeenSelected = this.resourceForm.controls['Country'].value !== "";
+    this.resourceForm
+      .get('Country')
+      .valueChanges.pipe(takeWhile(() => this.alive))
+      .subscribe(code => {
+        const currency = this.getCurrencyFromCode(code);
+        this.flag = this.getFlagForCountry(currency);
+        this.countryHasBeenSelected = code !== "";
+        if (code !== null) zipControl.enable();
+        if (code === 'CA') {
+          zipControl.setValidators(ValidateCAZip);
+        } else {
+          zipControl.setValidators(ValidateUSZip);
+        }
+      });
   }
 
   private setLocationID(): void {
@@ -51,5 +132,25 @@ export class SupplierLocationEditComponent implements OnChanges {
 
   handleAddressSelect(address) {
     this.selectAddress.emit(address);
+  }
+
+  ngOnDestroy(): void {
+    this.alive = false;
+  }
+
+  getControlValue(control: string): string | number {
+    return this.resourceForm?.controls[control]?.value;
+  }
+
+  getCountryFromCode(code: string): string {
+    return this.countryOptions.find(c => c.abbreviation === code)?.label;
+  }
+
+  getCurrencyFromCode = (code: string) => this.countryOptions.find(c => c.abbreviation === code)?.currency;
+
+  getFlagForCountry(currency: string): string {
+    if (currency === "") return undefined;
+    const flagMatch = this.currencyOptions?.find(c => c.Currency.toString() === currency)?.Icon;
+    if (flagMatch) return flagMatch;
   }
 }
