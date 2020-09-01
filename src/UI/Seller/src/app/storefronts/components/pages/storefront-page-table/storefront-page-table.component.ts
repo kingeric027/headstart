a9bcf05d96eea92e28f82c0dbdcb988ed65900e2
DJ Steinmetz <dsteinmetz@four51.com>
2020-09-01 16:33:45 -0500

    Fix height of page table/edit

--- src/UI/Seller/src/app/storefronts/components/pages/storefront-page-table/storefront-page-table.component.ts
+++ src/UI/Seller/src/app/storefronts/components/pages/storefront-page-table/storefront-page-table.component.ts
@@ -1,16 +1,19 @@
-import { Component, ChangeDetectorRef, NgZone } from '@angular/core';
+import { Component, ChangeDetectorRef, NgZone, AfterViewChecked } from '@angular/core';
 import { ResourceCrudComponent } from '@app-seller/shared/components/resource-crud/resource-crud.component';
 import { Router, ActivatedRoute } from '@angular/router';
 import { StorefrontsService } from '../../storefronts/storefronts.service';
 import { JDocument } from '@ordercloud/headstart-sdk';
 import { StorefrontPageService } from '../storefront-page.service';
+import { getPsHeight } from '@app-seller/shared/services/dom.helper';
+import { REDIRECT_TO_FIRST_PARENT } from '@app-seller/layout/header/header.config';
 @Component({
   selector: 'app-storefront-page-table',
   templateUrl: './storefront-page-table.component.html',
   styleUrls: ['./storefront-page-table.component.scss'],
 })
-export class StorefrontPageTableComponent extends ResourceCrudComponent<JDocument> {
+export class StorefrontPageTableComponent extends ResourceCrudComponent<JDocument> implements AfterViewChecked {
   route = 'pages';
+  resourceHeight: number;
   constructor(
     private storefrontPageService: StorefrontPageService,
     changeDetectorRef: ChangeDetectorRef,
@@ -21,7 +24,23 @@ export class StorefrontPageTableComponent extends ResourceCrudComponent<JDocumen
   ) {
     super(changeDetectorRef, storefrontPageService, router, activatedRoute, ngZone);
     console.log('resource in selection', this.resourceInSelection);
+    this.redirectToFirstParentIfNeeded();
   }
+
+  ngAfterViewChecked(): void {
+    this.resourceHeight = getPsHeight('additional-item-resource');
+  }
+
+  private async redirectToFirstParentIfNeeded() {
+    if (this.storefrontsService) {
+      const parentResourceID = await this.storefrontsService.getParentResourceID();
+      if (parentResourceID === REDIRECT_TO_FIRST_PARENT) {
+        await this.storefrontsService.listResources();
+        this.ocService.selectParentResource(this.storefrontsService.resourceSubject.value.Items[0]);
+      }
+    }
+  }
+
   resourceOrEmptyDoc(): JDocument {
     if (!this.resourceInSelection.ID) {
       // console.log(this.storefrontPageService.emptyResource);
