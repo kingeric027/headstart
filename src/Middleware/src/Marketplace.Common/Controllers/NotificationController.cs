c6c4b0b3c36bb5dd84e3f132beb664ff40996454
DJ Steinmetz <dsteinmetz@four51.com>
2020-10-13 15:43:50 -0500

    Add notification routes to middleware

---
+++ src/Middleware/src/Marketplace.Common/Controllers/NotificationController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Marketplace.Common.Commands;
+using Marketplace.Common.Commands.Crud;
+using Marketplace.Common.Models.Marketplace;
+using Marketplace.Models;
+using Marketplace.Models.Attributes;
+using Marketplace.Models.Misc;
+using Microsoft.AspNetCore.Mvc;
+using ordercloud.integrations.library;
+using OrderCloud.SDK;
+
+namespace Marketplace.Common.Controllers
+{
+	[DocComments("Notifications")]
+	[Route("notifications")]
+	public class NotificationController : BaseController
+	{
+
+		private readonly INotificationCommand _command;
+		public NotificationController(AppSettings settings, INotificationCommand command) : base(settings)
+		{
+			_command = command;
+		}
+
+		[DocName("POST Monitored Product Field Modified")]
+		[HttpPost, Route("monitored-product-field-modified"), OrderCloudIntegrationsAuth(ApiRole.ProductAdmin)]
+		public async Task<SuperMarketplaceProduct> CreateModifiedMonitoredSuperProductNotification([FromBody] MonitoredProductFieldModifiedNotification notification)
+		{
+			return await _command.CreateModifiedMonitoredSuperProductNotification(notification, VerifiedUserContext);
+		}
+	}
+}
