81afaf68327b1f05d7fb300e5e66a25c805d19af
DJ Steinmetz <dsteinmetz@four51.com>
2020-10-15 15:21:55 -0500

    Fix lint errors in component file

--- src/UI/Buyer/default-components/src/app/components/products/product-details/product-details.component.ts
+++ src/UI/Buyer/default-components/src/app/components/products/product-details/product-details.component.ts
@@ -98,21 +98,21 @@ export class OCMProductDetails implements OnInit {
   }
 
   getVariantInventory(): number {
-    let specCombo = "";
+    let specCombo = '';
     let specOptions: SpecOption[] = [];
     this._superProduct?.Specs?.forEach(s => s.Options.forEach(o => specOptions = specOptions.concat(o)));
-    for (var i = 0; i < this.specForm.value.ctrls.length; i++) {
+    for (let i = 0; i < this.specForm.value.ctrls.length; i++) {
       const matchingOption = specOptions.find(o => o.Value === this.specForm.value.ctrls[i])
       i === 0 ? specCombo += matchingOption.ID : specCombo += `-${matchingOption.ID}`
     }
     return this._superProduct.Variants.find(v => v.xp?.SpecCombo === specCombo)?.Inventory?.QuantityAvailable
   }
 
-  onSelectionInactive(event: boolean) {
+  onSelectionInactive(event: boolean): void {
     this.isInactiveVariant = event;
   }
 
-  populateInactiveVariants(superProduct: SuperMarketplaceProduct) {
+  populateInactiveVariants(superProduct: SuperMarketplaceProduct): void {
     this._disabledVariants = [];
     superProduct.Variants?.forEach(variant => {
       if (!variant.Active) {
