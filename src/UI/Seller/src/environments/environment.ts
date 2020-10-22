d1727931f38e4553d971b750ecd82ae03f156af5
Ryan Lauer <rdlauer@four51.com>
2020-10-22 15:59:15 -0500

    WIP get excel over the wire

--- src/UI/Seller/src/environments/environment.ts
+++ src/UI/Seller/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   hostedApp: false,
   sellerID: 'rQYR6T6ZTEqVrgv8x_ei0g',
   clientID: '06C93629-FE9A-4EC5-9652-C0F059B5CC7C',
-  middlewareUrl: 'https://marketplace-middleware-test.azurewebsites.net',
+  middlewareUrl: 'https://localhost:44340',
   appname: 'Marketplace Admin Local',
   translateBlobUrl: 'https://marktplacetest.blob.core.windows.net/ngx-translate/i18n/',
   blobStorageUrl: 'https://marktplacetest.blob.core.windows.net',
@@ -15,21 +15,20 @@ export const environment = {
   buyerConfigs: {
     'Anytime Fitness': {
       clientID: 'A5231DF1-2B00-4002-AB40-738A9E2CEC4B',
-      buyerUrl: 'https://marketplace-buyer-ui-test.azurewebsites.net/'
+      buyerUrl: 'https://marketplace-buyer-ui-test.azurewebsites.net/',
     },
     'Basecamp Fitness': {
       clientID: 'A5231DF1-2B00-4002-AB40-738A9E2CEC4B',
-      buyerUrl: 'https://marketplace-buyer-ui-test.azurewebsites.net/'
+      buyerUrl: 'https://marketplace-buyer-ui-test.azurewebsites.net/',
     },
     'The Bar Method': {
       clientID: 'A5231DF1-2B00-4002-AB40-738A9E2CEC4B',
-      buyerUrl: 'https://marketplace-buyer-ui-test.azurewebsites.net/'
+      buyerUrl: 'https://marketplace-buyer-ui-test.azurewebsites.net/',
     },
     'Waxing The City': {
       clientID: 'A5231DF1-2B00-4002-AB40-738A9E2CEC4B',
-      buyerUrl: 'https://marketplace-buyer-ui-test.azurewebsites.net/'
-    }
+      buyerUrl: 'https://marketplace-buyer-ui-test.azurewebsites.net/',
+    },
   },
-  superProductFieldsToMonitor: ['PriceSchedule', 'Product.xp.IsResale']
+  superProductFieldsToMonitor: ['PriceSchedule', 'Product.xp.IsResale'],
 };
-
