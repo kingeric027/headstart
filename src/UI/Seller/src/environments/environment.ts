1f5e5036e0e4d7e600547e6b40e5d07feacc2ff8
Ryan Lauer <rdlauer@four51.com>
2020-11-10 17:02:45 -0600

    cleanup

--- src/UI/Seller/src/environments/environment.ts
+++ src/UI/Seller/src/environments/environment.ts
@@ -23,8 +23,8 @@ type MiddlewareLocationSelection = LOCAL | TEST;
 // ===== MAKE CHANGES TO CONFIGURATION BETWEEN THESE LINES ONLY =======
 // ====================================================================
 const appName: AppName = SELF_ESTEEM_BRANDS;
-const middlewareLocationSelection: MiddlewareLocationSelection = LOCAL;
-const localMiddlewareURL = 'https://localhost:44340';
+const middlewareLocationSelection: MiddlewareLocationSelection = TEST;
+const localMiddlewareURL = 'https://localhost:44381';
 // ====================================================================
 // ======= UNLESS YOU ARE DOING SOMETHING WEIRD =======================
 
