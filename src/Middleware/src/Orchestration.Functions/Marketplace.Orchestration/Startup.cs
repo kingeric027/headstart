fb990738c50e646124d0a5ad56b18bf999ff78f4
Eric <eking@four51.com>
2020-11-16 13:44:55 -0600

    Add back IProductTemplateCommand injection

--- src/Middleware/src/Orchestration.Functions/Marketplace.Orchestration/Startup.cs
+++ src/Middleware/src/Orchestration.Functions/Marketplace.Orchestration/Startup.cs
@@ -67,6 +67,7 @@ namespace Marketplace.Orchestration
                 .Inject<IOrchestrationCommand>()
                 .Inject<ISupplierSyncCommand>()
                 .Inject<ISyncCommand>()
+                .Inject<IProductTemplateCommand>()
                 .Inject<ISendgridService>();
         }
     }
