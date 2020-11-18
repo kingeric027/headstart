569af2dc66b2d8e85d9134363aa2e3d8f9f591af
Ryan Lauer <rdlauer@four51.com>
2020-11-18 09:35:45 -0600

    more tests fixed

--- src/UI/Seller/src/app/buyers/components/approvals/buyer-approval-table/buyer-apporoval-table.component.spec.ts
+++ src/UI/Seller/src/app/buyers/components/approvals/buyer-approval-table/buyer-apporoval-table.component.spec.ts
@@ -1,18 +1,59 @@
+import { BuyerApprovalTableComponent } from './buyer-approval-table.component';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { SupplierTableComponent } from '@app-seller/suppliers/components/suppliers/supplier-table/supplier-table.component';
+import { BuyerApprovalService } from '../buyer-approval.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BehaviorSubject, from } from 'rxjs';
+import { BuyerService } from '../../buyers/buyer.service';
+import { ListPage } from '@ordercloud/angular-sdk';
+import { ResourceType } from '@ordercloud/angular-cms-components/shared/models/resource-type.interface';
 
-describe('SupplierTableComponent', () => {
-  let component: SupplierTableComponent;
-  let fixture: ComponentFixture<SupplierTableComponent>;
+describe('BuyerApprovalTableComponent', () => {
+  let component: BuyerApprovalTableComponent;
+  let fixture: ComponentFixture<BuyerApprovalTableComponent>;
 
+  const router = {
+    navigateByUrl: jasmine.createSpy('navigateByUrl'),
+    url: { startsWith() {} },
+    routerState: { snapshot: { url: 'url' } },
+  };
+  const resourceSubjectMock = new BehaviorSubject<ListPage<ResourceType>>(undefined);
+
+  const activatedRoute = { snapshot: { queryParams: {} }, params: from([{ id: 1 }]) };
+  const buyerApprovalService = {
+    isSupplierUser() {
+      return true;
+    },
+    getParentResourceID() {
+      return 1234;
+    },
+    resourceSubject: resourceSubjectMock.asObservable(),
+    getParentOrSecondaryIDParamName() {
+      return 'name';
+    },
+  };
+  const buyerService = {
+    isSupplierUser() {
+      return true;
+    },
+  };
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [SupplierTableComponent],
+      declarations: [BuyerApprovalTableComponent],
+      imports: [],
+      providers: [
+        { provide: BuyerApprovalService, useValue: buyerApprovalService },
+        { provide: Router, useValue: router },
+        {
+          provide: ActivatedRoute,
+          useValue: activatedRoute,
+        },
+        { provide: BuyerService, useValue: buyerService },
+      ],
     }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SupplierTableComponent);
+    fixture = TestBed.createComponent(BuyerApprovalTableComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
