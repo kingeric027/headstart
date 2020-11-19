7c1e402f93b7f8effcee0a7be553c95c7ff2cfd8
DJ Steinmetz <dsteinmetz@four51.com>
2020-11-19 17:15:44 -0600

    SEB-1261 - Individual product free shipping middleware

--- src/Middleware/src/Marketplace.Common/Models/Marketplace/MarketplaceProduct.cs
+++ src/Middleware/src/Marketplace.Common/Models/Marketplace/MarketplaceProduct.cs
@@ -83,6 +83,7 @@ namespace Marketplace.Models
 		public CurrencySymbol? Currency { get; set; } = null;
         public bool? ArtworkRequired { get; set; } = false;
         public bool PromotionEligible { get; set; }
+        public bool FreeShipping { get; set; }
     }
 
 	[JsonConverter(typeof(StringEnumConverter))]
