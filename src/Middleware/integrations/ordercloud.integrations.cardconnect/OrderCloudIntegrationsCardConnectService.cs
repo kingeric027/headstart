d0d66bf9deeb50bd64ac1db2cb80cf7f69aa0dce
Steve Davis <sdavis@four51.com>
2020-11-20 10:21:47 -0600

    Revert "different auth key for card connect CAD"
    This reverts commit 42fb932d9b6d60d01fc816c6f2db02ff9407dcf4.

--- src/Middleware/integrations/ordercloud.integrations.cardconnect/OrderCloudIntegrationsCardConnectService.cs
+++ src/Middleware/integrations/ordercloud.integrations.cardconnect/OrderCloudIntegrationsCardConnectService.cs
@@ -42,7 +42,7 @@ namespace ordercloud.integrations.cardconnect
 
         private IFlurlRequest Request(string resource, string currency = null)
         {
-            return _flurl.Request($"{resource}").WithHeader("Authorization", $"Basic {((currency == "USD") ? Config.Authorization : "c2VidmVuZG86cjZ6bUA0OWJjTCF3RFQ5dyNZTlA=")}");
+            return _flurl.Request($"{resource}").WithHeader("Authorization", $"Basic {((currency == "USD") ? Config.Authorization : "c2VidmVuZG9yNnptQDQ5YmNMIXdEVDl3I1lOUA==")}");
         }
 
         public async Task<CardConnectAccountResponse> Tokenize(CardConnectAccountRequest request)
