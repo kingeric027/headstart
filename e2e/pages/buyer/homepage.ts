5a762186e93dfc342d7b1df434ecf86359353baa
DJ Steinmetz <dsteinmetz@four51.com>
2020-11-25 12:17:00 -0600

    SEB-1274 - Add buyer homepage tests

--- e2e/pages/buyer/homepage.ts
+++ e2e/pages/buyer/homepage.ts
@@ -1,105 +1,12 @@
 /* eslint-disable prettier/prettier */
-import { Selector, t } from 'testcafe'
+import { Selector } from 'testcafe'
 import { createRegExp } from '../../helpers/regExp-helper'
 
 class Homepage {
-	accountDropdown: Selector
-	homepageLogo: Selector
 	featuredProductsH3: Selector
-	refineByH2: Selector
-	supplierTitleH1: Selector
-	productsLink: Selector
-	suppliersLink: Selector
-	myProfileLink: Selector
-	myAddressesLink: Selector
-	myLocationsLink: Selector
-	myCreditCardsLink: Selector
-	ordersDropdown: Selector
-	placedByMeLink: Selector
-	placedInMyLocationsLink: Selector
-	awaitingMyApprovalLink: Selector
 
 	constructor() {
-		this.accountDropdown = Selector('#accountDropdown').withText(
-			createRegExp('account')
-		)
-		this.homepageLogo = Selector('img').withAttribute('class', 'logo')
 		this.featuredProductsH3 = Selector('h3').withText(createRegExp('featured products'))
-		this.refineByH2 = Selector('h2').withText(createRegExp('refine by'))
-		// TODO: Add ID to the h1 element there so it can be identified regardless of 
-		// what word is used from the translation file (i.e. vendor, supplier, etc)
-		// this.supplierTitleH1 = Selector('h1').withText(createRegExp('refine by'))
-		this.productsLink = Selector('a').withText(createRegExp('products'))
-		// TODO: Add ID to this element so that we are able to select it regardless of translated text
-		this.suppliersLink = Selector('a').withText(createRegExp('suppliers'))
-		this.myProfileLink = Selector('a').withText(createRegExp('my profile'))
-		this.myAddressesLink = Selector('a').withText(
-			createRegExp('my addresses')
-		)
-		this.myLocationsLink = Selector('a').withText(
-			createRegExp('my locations')
-		)
-		this.myCreditCardsLink = Selector('a').withText(
-			createRegExp('my credit cards')
-		)
-		this.ordersDropdown = Selector('#account-dropdown').withText(
-			createRegExp('orders')
-		)
-		this.placedByMeLink = Selector('a').withText(createRegExp('placed by me'))
-		this.placedInMyLocationsLink = Selector('a').withText(
-			createRegExp('placed in my locations')
-		)
-		this.awaitingMyApprovalLink = Selector('a').withText(
-			createRegExp('awaiting my approval')
-		)
-	}
-
-	async clickHomepageLogo() {
-		await t.click(this.homepageLogo)
-	}
-
-	async clickAccountButton() {
-		await t.click(this.accountDropdown)
-	}
-
-	async clickOrdersButton() {
-		await t.click(this.ordersDropdown)
-	}
-
-	async clickMyProfileLink() {
-		await t.click(this.myProfileLink)
-	}
-
-	async clickMyAddressesLink() {
-		await t.click(this.myAddressesLink)
-	}
-
-	async clickMyLocationsLink() {
-		await t.click(this.myLocationsLink)
-	}
-
-	async clickMyCreditCardsLink() {
-		await t.click(this.myCreditCardsLink)
-	}
-
-	async clickPlacedByMeLink() {
-		await t.click(this.placedByMeLink)
-	}
-
-	async clickPlacedInMyLocationsLink() {
-		await t.click(this.placedInMyLocationsLink)
-	}
-
-	async clickAwaitingMyApprovalLink() {
-		await t.click(this.awaitingMyApprovalLink)
-	}
-
-	async clickProductsLink() {
-		await t.click(this.productsLink)
-	}
-
-	async clickSuppliersLink() {
-		await t.click(this.suppliersLink)
 	}
 }
 
--- e2e/tests/buyer/homepage-tests.ts
+++ e2e/tests/buyer/homepage-tests.ts
@@ -7,11 +7,14 @@ import {
     baseTestCleanup,
 } from '../../helpers/test-setup'
 import buyerHeaderPage from '../../pages/buyer/buyer-header-page'
-import productDetailPage from '../../pages/buyer/product-detail-page'
-import shoppingCartPage from '../../pages/buyer/shopping-cart-page'
-import checkoutPage from '../../pages/buyer/checkout-page'
-import loadingHelper from '../../helpers/loading-helper'
+import '../../helpers/loading-helper'
 import homepage from '../../pages/buyer/homepage'
+import productListPage from '../../pages/buyer/product-list-page'
+import supplierListPage from '../../pages/buyer/supplier-list-page'
+import myProfilePage from '../../pages/buyer/my-profile-page'
+import myAddressesPage from '../../pages/buyer/my-addresses-page'
+import myLocationsPage from '../../pages/buyer/my-locations-page'
+import myCreditCardsPage from '../../pages/buyer/my-credit-cards-page'
 
 const getLocation = ClientFunction(() => document.location.href)
 
@@ -32,25 +35,71 @@ fixture`Homepage Tests`
     })
     .page(testConfig.buyerAppUrl)
 
-test('Can I click brand hyperlink and be brought to homepage? | 0000', async t => {
-    await homepage.clickAccountButton();
-    await homepage.clickMyCreditCardsLink();
-    await homepage.clickHomepageLogo();
-    // Expect that you're brought to home
+test('Can I click brand hyperlink and be brought to homepage? | 2433', async t => {
+    await buyerHeaderPage.clickAccountButton();
+    await buyerHeaderPage.clickMyCreditCardsLink();
+    await buyerHeaderPage.clickHomepageBrandLogo();
+    // Assert that you're brought to home
     await t.expect(getLocation()).contains('home');
     await t.expect(homepage.featuredProductsH3.exists).ok()
 })
-test('Can I navigate to products list page? | 0000', async t => {
-    await homepage.clickProductsLink();
-    // Expect that you're brought to product list page
+
+test('Can I navigate to products list page? | 2434', async t => {
+    await buyerHeaderPage.clickProductsLink();
+    // Assert that you're brought to product list page
     await t.expect(getLocation()).contains('products');
-    await t.expect(homepage.refineByH2.exists).ok()
+    await t.expect(productListPage.products.exists).ok()
 })
-// TODO: Reinstate this test once an ID has been added
-// test('Can I navigate to suppliers list page? | 0000', async t => {
-//     await homepage.clickSuppliersLink();
-//     // Expect that you're brought to product list page
-//     await t.expect(getLocation()).contains('suppliers');
-//     // TODO: Add in expect h1 once ID is added to element
-//     // await t.expect(homepage.supplierTitleH1.exists).ok()
-// })
+
+test('Can I navigate to suppliers list page? | 2435', async t => {
+    await buyerHeaderPage.clickSuppliersLink();
+    // Assert that you're brought to product list page
+    await t.expect(getLocation()).contains('suppliers');
+    await t.expect(supplierListPage.supplierTitleH1.exists).ok()
+})
+
+test('Can I click the account dropdown and have all of the options populate? | 2466', async t => {
+    await buyerHeaderPage.clickAccountButton();
+    // Assert that all the dropdown options are populated
+    await t.expect(buyerHeaderPage.myProfileLink.exists).ok();
+    await t.expect(buyerHeaderPage.myAddressesLink.exists).ok();
+    await t.expect(buyerHeaderPage.myLocationsLink.exists).ok();
+    await t.expect(buyerHeaderPage.myCreditCardsLink.exists).ok();
+    await t.expect(buyerHeaderPage.logoutButton.exists).ok();
+})
+
+test('Can I click the my profile link and be brought to the my profile page? | 20072', async t => {
+    await buyerHeaderPage.clickAccountButton();
+    await buyerHeaderPage.clickMyProfileLink();
+    // Assert that you are brought to the my profile page
+    await t.expect(myProfilePage.accountDetailsH1.exists).ok();
+    await buyerHeaderPage.clickAccountButton();
+    await t.expect(buyerHeaderPage.myProfileLink.hasClass('active')).ok();
+})
+
+test('Can I click the my addresses link and be brought to the my addresses page? | 20073', async t => {
+    await buyerHeaderPage.clickAccountButton();
+    await buyerHeaderPage.clickMyAddressesLink();
+    // Assert that you are brought to the my addresses page
+    await t.expect(myAddressesPage.addressBookH1.exists).ok();
+    await buyerHeaderPage.clickAccountButton();
+    await t.expect(buyerHeaderPage.myAddressesLink.hasClass('active')).ok();
+})
+
+test('Can I click the my locations link and be brought to the my locations page? | 20074', async t => {
+    await buyerHeaderPage.clickAccountButton();
+    await buyerHeaderPage.clickMyLocationsLink();
+    // Assert that you are brought to the my locations page
+    await t.expect(myLocationsPage.locationsH1.exists).ok();
+    await buyerHeaderPage.clickAccountButton();
+    await t.expect(buyerHeaderPage.myLocationsLink.hasClass('active')).ok();
+})
+
+test('Can I click the my credit cards link and be brought to the my credit cards page? | 20075', async t => {
+    await buyerHeaderPage.clickAccountButton();
+    await buyerHeaderPage.clickMyCreditCardsLink();
+    // Assert that you are brought to the my credit cards page
+    await t.expect(myCreditCardsPage.paymentMethodsH1.exists).ok();
+    await buyerHeaderPage.clickAccountButton();
+    await t.expect(buyerHeaderPage.myCreditCardsLink.hasClass('active')).ok();
+})
