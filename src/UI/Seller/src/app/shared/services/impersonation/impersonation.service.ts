bdcbd707524ff9dfd32a9c35bc3f990ccaa20921
Max Maher <mmaher@four51.com>
2020-11-25 15:37:21 -0600

    SEB-1301 - Account for new custom roles

--- src/UI/Seller/src/app/shared/services/impersonation/impersonation.service.ts
+++ src/UI/Seller/src/app/shared/services/impersonation/impersonation.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, Inject } from '@angular/core';
-import { OcUserService, ApiRole, OcBuyerService } from '@ordercloud/angular-sdk';
+import { OcUserService, OcBuyerService } from '@ordercloud/angular-sdk';
 import { AppConfig, applicationConfiguration } from '@app-seller/config/app.config';
 
 @Injectable({
@@ -13,13 +13,19 @@ export class ImpersonationService {
   async impersonateUser(networkID: string, user: any): Promise<void> {
     const buyer = await this.buyerService.Get(networkID).toPromise();
     const buyerConfig = this.appConfig.buyerConfigs[buyer.Name];
+    const userCustomRoles = this.getCustomRolesForBuyerUser(user);
     const auth = await this.userService
       .GetAccessToken(networkID, user.ID, {
         ClientID: buyerConfig.clientID,
         Roles: this.appConfig.impersonatingBuyerScope,
+        CustomRoles: userCustomRoles
       })
       .toPromise();
     const url = buyerConfig.buyerUrl + 'impersonation?token=' + auth.access_token;
     window.open(url, '_blank');
+  }
+
+  getCustomRolesForBuyerUser(user: any): string[] {
+    return this.appConfig.impersonatingBuyerCustomRoleScope.filter(role => user.AvailableRoles.includes(role));
   }
 }
