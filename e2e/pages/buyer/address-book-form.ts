e157f0515c8c1377cd48d14b80e1f190f6f569ac
Max Maher <mmaher@four51.com>
2020-12-01 13:50:46 -0600

    SEB-1277 - Finished US/CA address tests

--- e2e/pages/buyer/address-book-form.ts
+++ e2e/pages/buyer/address-book-form.ts
@@ -3,90 +3,149 @@ import { Selector, t } from 'testcafe'
 import { createRegExp } from '../../helpers/regExp-helper'
 
 class AddressBookForm {
-    firstNameField: Selector
-    lastNameField: Selector
-    street1Field: Selector
-    street2Field: Selector
-    cityField: Selector
-    stateDropdown: Selector
-    stateOptions: Selector
-    zipField: Selector
-    phoneField: Selector
-    saveAddressButton: Selector
+	firstNameField: Selector
+	lastNameField: Selector
+	street1Field: Selector
+	street2Field: Selector
+	cityField: Selector
+	stateDropdown: Selector
+	stateOptions: Selector
+	zipField: Selector
+	phoneField: Selector
+	saveAddressButton: Selector
 
 	constructor() {
-        this.firstNameField = Selector('#FirstName').nth(1)
-        this.lastNameField = Selector('#LastName').nth(1)
-        this.street1Field = Selector('#Street1').nth(1)
-        this.street2Field = Selector('#Street2').nth(1)
-        this.cityField = Selector('#City').nth(1)
-        this.stateDropdown = Selector('#State').nth(1)
-        this.stateOptions = this.stateDropdown.find('option')
-        this.zipField = Selector('#zipCode').nth(1)
-        this.phoneField = Selector('#Phone').nth(1)
-        this.saveAddressButton = Selector('#address-save-button').nth(1)
+		this.firstNameField = Selector('#FirstName').nth(1)
+		this.lastNameField = Selector('#LastName').nth(1)
+		this.street1Field = Selector('#Street1').nth(1)
+		this.street2Field = Selector('#Street2').nth(1)
+		this.cityField = Selector('#City').nth(1)
+		this.stateDropdown = Selector('#State').nth(1)
+		this.stateOptions = this.stateDropdown.find('option')
+		this.zipField = Selector('#zipCode').nth(1)
+		this.phoneField = Selector('#Phone').nth(1)
+		this.saveAddressButton = Selector('#address-save-button').nth(1)
 	}
 
-    async enterFirstName(firstName: string) {
-        await t.typeText(this.firstNameField, firstName)
-    }
+    // Default addresses whenever adding
+	async enterDefaultUSAddress() {
+		await this.enterFirstName('Jane')
+		await this.enterLastName('Doe')
+		await this.enterStreet1('110 N 5th St')
+		await this.enterStreet2('Suite 300')
+		await this.enterCity('Minneapolis')
+		await this.selectState('MN')
+		await this.enterZip('55403')
+		await this.enterPhone('6515554545')
+	}
 
-    async removeFirstName() {
-        await t.click(this.firstNameField)
-        await t.pressKey('ctrl+a delete')
-    }
+	async enterDefaultCAAddress() {
+		await this.enterFirstName('Jane')
+		await this.enterLastName('Doe')
+		await this.enterStreet1('1150 Lorne Park Rd')
+		await this.enterCity('Mississauga')
+		await this.selectState('ON')
+		await this.enterZip('L5H 3A7')
+		await this.enterPhone('2265554545')
+	}
 
-    async enterLastName(lastName: string) {
-        await t.typeText(this.lastNameField, lastName)
-    }
+    // First Name
+	async enterFirstName(firstName: string) {
+		await t.typeText(this.firstNameField, firstName)
+	}
 
-    async removeLastName() {
-        await t.click(this.lastNameField)
-        await t.pressKey('ctrl+a delete')
-    }
+	async editFirstName(firstName: string) {
+		await t.typeText(this.firstNameField, firstName, { replace: true })
+	}
 
-    async enterStreet1(street1: string) {
-        await t.typeText(this.street1Field, street1)
-    }
+	async removeFirstName() {
+		await t.click(this.firstNameField)
+		await t.pressKey('ctrl+a delete')
+	}
 
-    async removeStreet1() {
-        await t.click(this.street1Field)
-        await t.pressKey('ctrl+a delete')
-    }
+    // Last Name
+	async enterLastName(lastName: string) {
+		await t.typeText(this.lastNameField, lastName)
+	}
 
-    async enterStreet2(street2: string) {
-        await t.typeText(this.street2Field, street2)
-    }
+	async editLastName(lastName: string) {
+		await t.typeText(this.lastNameField, lastName, { replace: true })
+	}
 
-    async enterCity(city: string) {
-        await t.typeText(this.cityField, city)
-    }
+	async removeLastName() {
+		await t.click(this.lastNameField)
+		await t.pressKey('ctrl+a delete')
+	}
 
-    async enterState(state: string) {
-        await t.click(this.stateDropdown)
-        await t.click(this.stateOptions.withText(createRegExp(state)))
-    }
+    // Street 1
+	async enterStreet1(street1: string) {
+		await t.typeText(this.street1Field, street1)
+	}
 
-    async enterZip(zip: string) {
-        await t.typeText(this.zipField, zip)
-    }
+	async editStreet1(street1: string) {
+		await t.typeText(this.street1Field, street1, { replace: true })
+	}
 
-    async enterPhone(phone: string) {
-        await t.typeText(this.phoneField, phone)
-    }
+	async removeStreet1() {
+		await t.click(this.street1Field)
+		await t.pressKey('ctrl+a delete')
+	}
 
-    async removeZip() {
-        await t.click(this.zipField)
-        await t.pressKey('ctrl+a delete')
-    }
+    // Street 2
+	async enterStreet2(street2: string) {
+		await t.typeText(this.street2Field, street2)
+	}
 
-    async clickSaveAddressButton() {
-        await t.click(this.saveAddressButton)
-    }
+	async editStreet2(street2: string) {
+		await t.typeText(this.street2Field, street2, { replace: true })
+	}
 
-    async isButtonDisabled() {
-        await t.expect(this.saveAddressButton.hasAttribute('disabled')).ok()
-    }
+    // City
+	async enterCity(city: string) {
+		await t.typeText(this.cityField, city)
+	}
+
+	async editCity(city: string) {
+		await t.typeText(this.cityField, city, { replace: true })
+	}
+
+    // State
+	async selectState(state: string) {
+		await t.click(this.stateDropdown)
+		await t.click(this.stateOptions.withText(createRegExp(state)))
+	}
+
+    // Zip
+	async enterZip(zip: string) {
+		await t.typeText(this.zipField, zip)
+	}
+
+	async editZip(zip: string) {
+		await t.typeText(this.zipField, zip, { replace: true })
+	}
+
+	async removeZip() {
+		await t.click(this.zipField)
+		await t.pressKey('ctrl+a delete')
+	}
+
+    // Phone
+	async enterPhone(phone: string) {
+		await t.typeText(this.phoneField, phone)
+	}
+
+	async editPhone(phone: string) {
+		await t.typeText(this.phoneField, phone, { replace: true })
+	}
+
+    // Save
+	async clickSaveAddressButton() {
+		await t.click(this.saveAddressButton)
+	}
+
+	async isButtonDisabled() {
+		await t.expect(this.saveAddressButton.hasAttribute('disabled')).ok()
+	}
 }
 
 export default new AddressBookForm()
--- e2e/pages/buyer/address-book-page.ts
+++ e2e/pages/buyer/address-book-page.ts
@@ -3,27 +3,67 @@ import { Selector, t } from 'testcafe'
 import { createRegExp } from '../../helpers/regExp-helper'
 
 class AddressBookPage {
-    addAddressButton: Selector
-    address: Selector
-    smartyStreetsSuggestionHeader: Selector
+	addAddressButton: Selector
+	address: Selector
+	smartyStreetsSuggestionHeader: Selector
+	confirmAddressDeleteButton: Selector
 
 	constructor() {
-        this.addAddressButton = Selector('#add-address-button')
-        this.address = Selector('ocm-address-card').nth(1)
-        this.smartyStreetsSuggestionHeader = Selector('address-suggestion')
+		this.addAddressButton = Selector('#add-address-button')
+		this.address = Selector('ocm-address-card').nth(1)
+		this.smartyStreetsSuggestionHeader = Selector('address-suggestion')
+		this.confirmAddressDeleteButton = Selector('#confirm-address-delete')
 	}
 
 	async clickAddAddressButton() {
-        await t.click(this.addAddressButton)
-    }
+		await t.click(this.addAddressButton)
+	}
 
-    async addressExists(address: string) {
-        await this.address()
-        const shadowAddressDataExists = Selector(() => document.querySelectorAll('ocm-address-card')[1]
-            .shadowRoot.querySelector('.address-detail'))
-            .withText(createRegExp(address)).exists
-        return shadowAddressDataExists
-    }
+	async clickDeleteAddressButton() {
+		const slottedAddressDeleteButton = Selector(() =>
+			document
+				.querySelectorAll('ocm-address-card')[1]
+				.querySelector('.address-delete')
+		)
+		await t.click(slottedAddressDeleteButton)
+	}
+
+	async clickConfirmDeleteAddressButton() {
+		await t.click(this.confirmAddressDeleteButton)
+	}
+
+	async clickEditAddressButton() {
+		const slottedAddressEditButton = Selector(() =>
+			document
+				.querySelectorAll('ocm-address-card')[1]
+				.querySelector('.address-edit')
+		)
+		await t.click(slottedAddressEditButton)
+	}
+
+	async addedOrEditedAddressExists(address: string) {
+		await this.address()
+		const shadowAddressDataExists = Selector(() =>
+			document
+				.querySelectorAll('ocm-address-card')[1]
+				.shadowRoot.querySelector('.address-detail')
+		).withText(createRegExp(address)).exists
+		return shadowAddressDataExists
+	}
+
+	async suggestedAddressExists() {
+		const shadowAddressDataExists = Selector(() =>
+			document
+				.querySelectorAll('ocm-address-card')[1]
+				.shadowRoot.querySelector('.address-detail')
+		).exists
+		return shadowAddressDataExists
+	}
+
+	async deletedAddressExists() {
+		return Selector(() => document.querySelectorAll('ocm-address-card')[1])
+			.exists
+	}
 }
 
 export default new AddressBookPage()
