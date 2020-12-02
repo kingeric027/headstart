8f641aa0eb5830d5adefc30690fc0905ae43034c
Ryan Lauer <rdlauer@four51.com>
2020-12-02 17:29:02 -0600

    130 specs, 11 failures

--- src/UI/Seller/src/app/buyers/components/buyers/buyer-table/buyer-table.component.spec.ts
+++ src/UI/Seller/src/app/buyers/components/buyers/buyer-table/buyer-table.component.spec.ts
@@ -1,22 +1,51 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClient } from '@angular/common/http'
+import { async, ComponentFixture, TestBed } from '@angular/core/testing'
+import { ActivatedRoute, Router } from '@angular/router'
+import { of } from 'rxjs'
+import { BuyerService } from '../buyer.service'
 
-import { BuyerTableComponent } from './buyer-table.component';
+import { BuyerTableComponent } from './buyer-table.component'
 
 describe('BuyerTableComponent', () => {
-  let component: BuyerTableComponent;
-  let fixture: ComponentFixture<BuyerTableComponent>;
+  let component: BuyerTableComponent
+  let fixture: ComponentFixture<BuyerTableComponent>
+
+  const router = {
+    navigateByUrl: jasmine.createSpy('navigateByUrl'),
+    url: '/my-',
+  }
+  const activatedRoute = {}
+  const httpClient = {}
+  const buyerService = {
+    isSupplierUser() {
+      return true
+    },
+    getMyResource() {
+      return {}
+    },
+    copyResource() {},
+    resourceSubject: of({}),
+  }
 
   beforeEach(async(() => {
-    TestBed.configureTestingModule({ declarations: [BuyerTableComponent] }).compileComponents();
-  }));
+    TestBed.configureTestingModule({
+      declarations: [BuyerTableComponent],
+      providers: [
+        { provide: Router, useValue: router },
+        { provide: ActivatedRoute, useValue: activatedRoute },
+        { provide: HttpClient, useValue: httpClient },
+        { provide: BuyerService, useValue: buyerService },
+      ],
+    }).compileComponents()
+  }))
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(BuyerTableComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(BuyerTableComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
--- src/UI/Seller/src/app/products/components/product-edit/product-edit.component.spec.ts
+++ src/UI/Seller/src/app/products/components/product-edit/product-edit.component.spec.ts
@@ -1,21 +1,43 @@
-import { ProductEditComponent } from './product-edit.component';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ProductEditComponent } from './product-edit.component'
+import { async, ComponentFixture, TestBed } from '@angular/core/testing'
+import { ChangeDetectorRef, NgZone } from '@angular/core'
+import { Router, ActivatedRoute } from '@angular/router'
+import { AppAuthService } from '@app-seller/auth'
+import { OrderService } from '@app-seller/orders/order.service'
+import { of } from 'rxjs'
 
 describe('ProductEditComponent', () => {
-  let component: ProductEditComponent;
-  let fixture: ComponentFixture<ProductEditComponent>;
+  let component: ProductEditComponent
+  let fixture: ComponentFixture<ProductEditComponent>
+
+  const router = {}
+  const orderService = { resourceSubject: of({}) }
+  const changeDetectorRef = {}
+  const activatedRoute = { queryParams: of({}), params: of({}) }
+  const ngZone = {}
+  const appAuthService = {}
 
   beforeEach(async(() => {
-    TestBed.configureTestingModule({ declarations: [ProductEditComponent] }).compileComponents();
-  }));
+    TestBed.configureTestingModule({
+      declarations: [ProductEditComponent],
+      providers: [
+        { provide: Router, useValue: router },
+        { provide: OrderService, useValue: orderService },
+        { provide: ChangeDetectorRef, useValue: changeDetectorRef },
+        { provide: ActivatedRoute, useValue: activatedRoute },
+        { provide: NgZone, useValue: ngZone },
+        { provide: AppAuthService, useValue: appAuthService },
+      ],
+    }).compileComponents()
+  }))
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ProductEditComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(ProductEditComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
