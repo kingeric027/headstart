d5c36d85b5ec3d7dc0dddb02125fef962d9b9bac
Oliver Heywood <oheywood@four51.com>
2020-12-03 13:46:54 -0600

    another wack at trying to reduce news

--- src/Middleware/integrations/ordercloud.integrations.cms/Storage/StorageHelper.cs
+++ src/Middleware/integrations/ordercloud.integrations.cms/Storage/StorageHelper.cs
@@ -5,68 +5,53 @@ using System.Threading.Tasks;
 using ordercloud.integrations.library;
 using System.Drawing;
 using System.Drawing.Imaging;
-using Microsoft.WindowsAzure.Storage;
+using System.Collections.Generic;
 
 namespace ordercloud.integrations.cms
 {
 	public static class StorageHelper
 	{
+		// Keys are container ids. 
+		private static Dictionary<string, OrderCloudIntegrationsBlobService> storageConnections = new Dictionary<string, OrderCloudIntegrationsBlobService>();
+		
 		public static async Task UploadFile(AssetContainer container, string blobName, IFormFile file)
 		{
-			await RunAction(container, async blobs =>
-			{
-				var block = blobs.GetBlockBlobReference(blobName);
-				block.Properties.ContentType = file.ContentType;
-				using (var stream = file.OpenReadStream())
-				{
-					await block.UploadFromStreamAsync(stream);
-				}	
-			});
+			await RunAction(container, blobs => blobs.Save(blobName, file));
 		}
 
 		public static async Task UploadImage(AssetContainer container, string blobName, Image image)
 		{
-			await RunAction(container, async blobs =>
-			{
+			await RunAction(container, async blobs => {
 				var bytes = image.ToBytes(ImageFormat.Png);
-				var block = blobs.GetBlockBlobReference(blobName);
-				block.Properties.ContentType = "image/png";
-				await block.UploadFromByteArrayAsync(bytes, 0, bytes.Length);
+				await blobs.Save(blobName, bytes, "image/png");
 			});
 		}
 
 		public static async Task DeleteAsset(AssetContainer container, string blobName)
 		{
-			await RunAction(container, blobs => blobs.GetBlockBlobReference(blobName).DeleteIfExistsAsync());
+			await RunAction(container, blobs => blobs.Delete(blobName));
 		}
 
-		private static async Task RunAction(AssetContainer assetContainer, Func<CloudBlobContainer, Task> action)
+		private static async Task RunAction(AssetContainer container, Func<OrderCloudIntegrationsBlobService, Task> action)
 		{
-			var blobContainer = await GetBlobContainer(assetContainer);
+			if (!storageConnections.ContainsKey(container.id))
+			{
+				var blobService = new OrderCloudIntegrationsBlobService(new BlobServiceConfig()
+				{
+					ConnectionString = container.Customer.StorageConnectionString,
+					Container = $"assets-{container.id}", // SellerOrgID can contain "_", an illegal character for blob containers.
+					AccessType = BlobContainerPublicAccessType.Container
+				});
+				storageConnections.Add(container.id, blobService);
+			}
 			try
 			{
-				await action(blobContainer);
+				await action(storageConnections[container.id]);
 			}
 			catch (Exception ex)
 			{
-				throw new StorageConnectionException(assetContainer.id, ex);
+				throw new StorageConnectionException(container.id, ex);
 			}
-		}
-
-		private static async Task<CloudBlobContainer> GetBlobContainer(AssetContainer assetContainer)
-		{
-			var containerName = $"assets-{assetContainer.id}";
-			CloudStorageAccount.TryParse(assetContainer.Customer.StorageConnectionString, out var storage);
-			var client = storage.CreateCloudBlobClient();
-			var blobContainer = client.GetContainerReference(containerName);
-			var isNewlyCreated = await blobContainer.CreateIfNotExistsAsync();
-			if (isNewlyCreated)
-			{
-				var permissions = await blobContainer.GetPermissionsAsync();
-				permissions.PublicAccess = BlobContainerPublicAccessType.Blob;
-				await blobContainer.SetPermissionsAsync(permissions);
-			}
-			return blobContainer;
 		}
 	}
 }
