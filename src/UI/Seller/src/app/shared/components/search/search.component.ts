568d6dc78a23e052c62119450507444bd3813014
Adam Zelmer <azelmer@four51.com>
2020-12-11 13:43:36 -0600

    SEB-1364 increase debounce time to 800 ms

--- src/UI/Seller/src/app/shared/components/search/search.component.ts
+++ src/UI/Seller/src/app/shared/components/search/search.component.ts
@@ -58,7 +58,7 @@ export class SearchComponent implements OnInit, OnChanges, OnDestroy {
           const userTriggered = this.form.dirty
           return searchTerm !== this.previousSearchTerm && userTriggered
         }),
-        debounceTime(500),
+        debounceTime(800),
         takeWhile(() => this.alive)
       )
       .subscribe((searchTerm) => {
