55fc52e878cdc3028343aaba27917a754a6890a2
Crhistian Ramirez <cramirez@four51.com>
2020-12-22 10:06:37 -0600

    SEB-1408 do not process transaction if it exists

--- src/Middleware/integrations/ordercloud.integrations.cardconnect/OrderCloudIntegrationsCardConnectCommand.cs
+++ src/Middleware/integrations/ordercloud.integrations.cardconnect/OrderCloudIntegrationsCardConnectCommand.cs
@@ -1,4 +1,4 @@
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using ordercloud.integrations.library;
 using OrderCloud.SDK;
@@ -60,7 +60,10 @@ namespace ordercloud.integrations.cardconnect
 			var ocPayment = await _oc.Payments.GetAsync<Payment>(OrderDirection.Incoming, payment.OrderID, payment.PaymentID);
             try
             {
-                var call = await _cardConnect.AuthWithCapture(CardConnectMapper.Map(cc, order, payment, merchantID, ccAmount));
+				if(ocPayment.Accepted == true)
+                {
+					return ocPayment;
+                }
                 var call = await _cardConnect.AuthWithoutCapture(CardConnectMapper.Map(cc, order, payment, merchantID, ccAmount));
                 ocPayment = await _oc.Payments.PatchAsync(OrderDirection.Incoming, order.ID, ocPayment.ID, new PartialPayment {Accepted = true, Amount = ccAmount});
                 return await _oc.Payments.CreateTransactionAsync(OrderDirection.Incoming, order.ID, ocPayment.ID, CardConnectMapper.Map(order, ocPayment, call));
