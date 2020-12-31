3e420cbaaf6f5dc56de87e721a6dce9a26ef0f36
Alexa Snyder <asnyder@four51.com>
2020-12-31 17:31:44 -0600

    SEB-834: add form to template

--- src/UI/Seller/src/app/support/case-submission/case-submission.component.ts
+++ src/UI/Seller/src/app/support/case-submission/case-submission.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CurrentUserService } from '@app-seller/shared/services/current-user/current-user.service';
+import { MeUser } from '@ordercloud/angular-sdk';
+import { takeWhile } from 'rxjs/operators';
 
 @Component({
   selector: 'support-case-submission',
@@ -6,10 +10,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./case-submission.component.scss']
 })
 export class CaseSubmissionComponent implements OnInit {
+  alive = true
+  caseSubmissionForm: FormGroup
+  subjectOptions: string[] = [
+    'General Question',
+    'Report an Error/Bug',
+    'Payment, Billing, or Refunds'
+  ]
+  user: MeUser
+  
 
-  constructor() { }
+  constructor(private currentUserService: CurrentUserService, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.currentUserService.userSubject.pipe(takeWhile(() => this.alive)).subscribe((user) => {
+      this.user = user
+      this.setForm()
+    })
+  }
+
+  setForm(): void {
+    this.caseSubmissionForm = this.formBuilder.group({
+      FirstName: ['', Validators.required],
+      LastName: ['', Validators.required],
+      Email: ['', Validators.required],
+      Vendor: ['', Validators.required],
+      Subject: [null, Validators.required],
+      Message: ['', Validators.required]
+    })
+  }
+
+  sendCaseSubmission(): void {
+    console.log('sent!')
+  }
+
+  ngOnDestroy(): void {
+    this.alive = false
   }
 
 }
