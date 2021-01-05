cf920a822e8a7527aae08fd79994853c107c5cd7
Max Maher <mmaher@four51.com>
2021-01-05 12:58:04 -0600

    SEB-1409 - Null checks added when assigning PO Shipping Address

--- src/Middleware/src/Marketplace.Common/Commands/PostSubmitCommand.cs
+++ src/Middleware/src/Marketplace.Common/Commands/PostSubmitCommand.cs
@@ -372,16 +372,16 @@ namespace Marketplace.Common.Commands
                         // ShippingAddress needed for Purchase Order Detail Report
                         ShippingAddress = new MarketplaceAddressBuyer()
                         {
-                            ID = buyerOrder.Order.xp.ShippingAddress.ID,
-                            CompanyName = buyerOrder.Order.xp.ShippingAddress.CompanyName,
-                            FirstName = buyerOrder.Order.xp.ShippingAddress.FirstName,
-                            LastName = buyerOrder.Order.xp.ShippingAddress.LastName,
-                            Street1 = buyerOrder.Order.xp.ShippingAddress.Street1,
-                            Street2 = buyerOrder.Order.xp.ShippingAddress.Street2,
-                            City = buyerOrder.Order.xp.ShippingAddress.City,
-                            State = buyerOrder.Order.xp.ShippingAddress.State,
-                            Zip = buyerOrder.Order.xp.ShippingAddress.Zip,
-                            Country = buyerOrder.Order.xp.ShippingAddress.Country,
+                            ID = buyerOrder?.Order?.xp?.ShippingAddress?.ID,
+                            CompanyName = buyerOrder?.Order?.xp?.ShippingAddress?.CompanyName,
+                            FirstName = buyerOrder?.Order?.xp?.ShippingAddress?.FirstName,
+                            LastName = buyerOrder?.Order?.xp?.ShippingAddress?.LastName,
+                            Street1 = buyerOrder?.Order?.xp?.ShippingAddress?.Street1,
+                            Street2 = buyerOrder?.Order?.xp?.ShippingAddress?.Street2,
+                            City = buyerOrder?.Order?.xp?.ShippingAddress?.City,
+                            State = buyerOrder?.Order?.xp?.ShippingAddress?.State,
+                            Zip = buyerOrder?.Order?.xp?.ShippingAddress?.Zip,
+                            Country = buyerOrder?.Order?.xp?.ShippingAddress?.Country,
                         }
             }
                 };
