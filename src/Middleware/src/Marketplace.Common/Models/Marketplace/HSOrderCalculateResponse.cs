6e140ed7afef968fa779532cc4bf98b227e5e8b7
Eric <eking@four51.com>
2021-01-06 12:38:43 -0600

    Add back in HSOrderCalculateResponse

---
+++ src/Middleware/src/Marketplace.Common/Models/Marketplace/HSOrderCalculateResponse.cs
@@ -0,0 +1,17 @@
+﻿using Avalara.AvaTax.RestClient;
+using OrderCloud.SDK;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Marketplace.Common.Models.Marketplace
+{
+    public class HSOrderCalculateResponse : OrderCalculateResponse<OrderCalculateResponseXp>
+    {
+    }
+
+    public class OrderCalculateResponseXp
+    {
+        public TransactionModel TaxResponse { get; set; }
+    }
+}
